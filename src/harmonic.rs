fdc666090428282889bb8aa32c1d93c891b5cc72
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-09-02 15:54:50 -0400

    Update harmonic.rs

--- src/harmonic.rs
+++ src/harmonic.rs
@@ -9,7 +9,7 @@ pub struct Harmonic {
 impl Harmonic {
     pub fn new() -> Self {
         Self {
-            ctr: BigRational::from_integer(BigInt::from(1)),
+            ctr: BigRational::from_integer(BigInt::from(0)),
         }
     }
 }
@@ -23,6 +23,34 @@ impl Iterator for Harmonic {
     }
 }
 
+/// The partial sums of the harmonic series.
+/// 0, 1, 3/2, 11/6, 25/12, 137/60, 49/20, 363/140...
+pub struct HarmonicSums {
+    val: BigRational,
+    ctr: BigRational,
+}
+
+impl HarmonicSums {
+    pub fn new() -> Self {
+        Self {
+            val: BigRational::from_integer(BigInt::from(0)),
+            ctr: BigRational::from_integer(BigInt::from(1)),
+        }
+    }
+}
+
+impl Iterator for HarmonicSums {
+    type Item = BigRational;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let out = self.val.clone();
+        self.val += self.ctr.recip();
+        self.ctr += BigRational::from_integer(BigInt::from(1));
+        Some(out)
+    }
+}
+
 crate::print_a_few!(
     Harmonic::new(), 0, 10;
+    HarmonicSums::new(), 0, 8;
 );
