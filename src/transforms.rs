0027b5c81c3a9e8fecd3995681b23bbf256b8651
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-09-02 14:17:53 -0400

    better generic transforms

--- src/transforms.rs
+++ src/transforms.rs
@@ -1,6 +1,6 @@
-use num::{One, Zero};
-use std::ops::{Add, AddAssign, Mul, MulAssign};
+use num::{CheckedAdd, CheckedMul, One, Zero, rational::Ratio};
 
+/// Sequence of partial sums. Returns None if overflow occurs or sequence ends.
 pub struct PartialSums<T> {
     sum: T,
     iter: Box<dyn Iterator<Item = T>>,
@@ -18,16 +18,17 @@ impl<T: Zero> PartialSums<T> {
     }
 }
 
-impl<T: Add + AddAssign + Clone> Iterator for PartialSums<T> {
+impl<T: CheckedAdd + Clone> Iterator for PartialSums<T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
         let out = self.sum.clone();
-        self.sum += self.iter.next()?;
+        self.sum = self.sum.checked_add(&self.iter.next()?)?;
         Some(out)
     }
 }
 
+/// Sequence of partial products. Returns None if overflow occurs or sequence ends.
 pub struct PartialProds<T> {
     prod: T,
     iter: Box<dyn Iterator<Item = T>>,
@@ -45,12 +46,60 @@ impl<T: One> PartialProds<T> {
     }
 }
 
-impl<T: Mul + MulAssign + Clone> Iterator for PartialProds<T> {
+impl<T: CheckedMul + Clone> Iterator for PartialProds<T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
         let out = self.prod.clone();
-        self.prod *= self.iter.next()?;
+        self.prod = self.prod.checked_mul(&self.iter.next()?)?;
         Some(out)
+    }
+}
+
+/// Sequence of numerators of a sequence of ratios.
+pub struct Numerators<T> {
+    iter: Box<dyn Iterator<Item = Ratio<T>>>,
+}
+
+impl<T: One> Numerators<T> {
+    pub fn new<I>(iter: I) -> Self
+    where
+        I: Iterator<Item = Ratio<T>> + 'static,
+    {
+        Self {
+            iter: Box::new(iter),
+        }
+    }
+}
+
+impl<T: Clone> Iterator for Numerators<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(self.iter.next()?.numer().clone())
+    }
+}
+
+/// Sequence of denominators of a sequence of ratios.
+pub struct Denominator<T> {
+    iter: Box<dyn Iterator<Item = Ratio<T>>>,
+}
+
+impl<T: One> Denominator<T> {
+    pub fn new<I>(iter: I) -> Self
+    where
+        I: Iterator<Item = Ratio<T>> + 'static,
+    {
+        Self {
+            iter: Box::new(iter),
+        }
+    }
+}
+
+impl<T: Clone> Iterator for Denominator<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(self.iter.next()?.denom().clone())
     }
 }
