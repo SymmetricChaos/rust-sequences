cb7795f7b8ffc42105e48b9f62bf07032a769d74
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-09-03 12:45:19 -0400

    general (but still slow) smooth numbers

--- src/smooth.rs
+++ src/smooth.rs
@@ -1,11 +1,24 @@
+use itertools::Itertools;
 use num::{BigInt, Integer, One};
 
+use crate::primes::Prime;
+
 /// The regular numbers, fhose for which the only prime divisors are less than n.
-pub struct Smooth {}
+pub struct Smooth {
+    ctr: BigInt,
+    primes: Vec<BigInt>,
+}
 
 impl Smooth {
-    pub fn new() -> Self {
-        Self {}
+    /// Panics if n is less than two.
+    pub fn new(n: i64) -> Self {
+        assert!(n > 1);
+        Self {
+            ctr: BigInt::from(0),
+            primes: Prime::new()
+                .take_while(|x| *x <= BigInt::from(n))
+                .collect_vec(),
+        }
     }
 }
 
@@ -13,7 +26,18 @@ impl Iterator for Smooth {
     type Item = BigInt;
 
     fn next(&mut self) -> Option<BigInt> {
-        todo!()
+        loop {
+            self.ctr += 1;
+            let mut n = self.ctr.clone();
+            for p in self.primes.iter() {
+                while n.is_multiple_of(p) {
+                    n = n / p
+                }
+            }
+            if n.is_one() {
+                return Some(self.ctr.clone());
+            }
+        }
     }
 }
 
@@ -60,4 +84,5 @@ crate::check_time!(
 
 crate::check_sequences!(
     Regular::new(), 9, 10, [12, 15, 16, 18, 20, 24, 25, 27, 30, 32];
+    Smooth::new(5), 9, 10, [12, 15, 16, 18, 20, 24, 25, 27, 30, 32];
 );
