5159a4be1187a5032b05ee20c92e233a22d26400
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-09-12 13:23:25 -0400

    simpler continued fraction

--- src/core/continued_fraction.rs
+++ src/core/continued_fraction.rs
@@ -1,4 +1,4 @@
-use num::{BigInt, CheckedAdd, CheckedMul, Integer, One, Zero, rational::Ratio};
+use num::{CheckedAdd, CheckedMul, Integer, rational::Ratio};
 
 /// Produce the convergents of a continued fraction given integer sequences representing the partial numerators and partial denominators.
 pub struct ContinuedFraction<T> {
@@ -10,7 +10,7 @@ pub struct ContinuedFraction<T> {
     dens: Box<dyn Iterator<Item = T>>,
 }
 
-impl<T: Clone + Integer> ContinuedFraction<T> {
+impl<T: Clone + Integer + 'static> ContinuedFraction<T> {
     pub fn new<N, D>(n: N, mut d: D) -> Self
     where
         N: Iterator<Item = T> + 'static,
@@ -23,6 +23,32 @@ impl<T: Clone + Integer> ContinuedFraction<T> {
             b1: T::one(),
             nums: Box::new(n),
             dens: Box::new(d),
+        }
+    }
+
+    pub fn new_periodic(
+        fixed_n: Vec<T>,
+        fixed_d: Vec<T>,
+        periodic_n: Vec<T>,
+        periodic_d: Vec<T>,
+    ) -> Self {
+        Self {
+            a0: T::one(),
+            b0: T::one(),
+            a1: fixed_d[0].clone(),
+            b1: T::one(),
+            nums: Box::new(
+                fixed_n
+                    .into_iter()
+                    .chain(periodic_n.into_iter().cycle())
+                    .skip(1),
+            ),
+            dens: Box::new(
+                fixed_d
+                    .into_iter()
+                    .chain(periodic_d.into_iter().cycle())
+                    .skip(1),
+            ),
         }
     }
 }
@@ -61,7 +87,7 @@ pub struct SimpleContinuedFraction<T> {
     dens: Box<dyn Iterator<Item = T>>,
 }
 
-impl<T: Clone + Integer> SimpleContinuedFraction<T> {
+impl<T: Clone + Integer + 'static> SimpleContinuedFraction<T> {
     pub fn new<I>(mut d: I) -> Self
     where
         I: Iterator<Item = T> + 'static,
@@ -74,44 +100,13 @@ impl<T: Clone + Integer> SimpleContinuedFraction<T> {
             dens: Box::new(d),
         }
     }
-}
 
-impl<T: Clone + Integer + CheckedAdd + CheckedMul> Iterator for SimpleContinuedFraction<T> {
-    type Item = Ratio<T>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let out = Ratio::new(self.a1.clone(), self.b1.clone());
-
-        let d = self.dens.next()?;
-
-        let a2 = d.checked_mul(&self.a1)?.checked_add(&self.a0)?;
-        let b2 = d.checked_mul(&self.b1)?.checked_add(&self.b0)?;
-
-        self.a0 = self.a1.clone();
-        self.b0 = self.b1.clone();
-        self.a1 = a2;
-        self.b1 = b2;
-
-        Some(out)
-    }
-}
-
-/// Produce the convergents of a simple continued fraction given an integer sequence representing the partial denominators.
-pub struct SimplePeriodicContinuedFraction {
-    a0: BigInt,
-    b0: BigInt,
-    a1: BigInt,
-    b1: BigInt,
-    dens: Box<dyn Iterator<Item = BigInt>>,
-}
-
-impl SimplePeriodicContinuedFraction {
-    pub fn new(fixed: Vec<BigInt>, periodic: Vec<BigInt>) -> Self {
+    pub fn new_periodic(fixed: Vec<T>, periodic: Vec<T>) -> Self {
         Self {
-            a0: BigInt::one(),
-            b0: BigInt::zero(),
+            a0: T::one(),
+            b0: T::zero(),
             a1: fixed[0].clone(),
-            b1: BigInt::one(),
+            b1: T::one(),
             dens: Box::new(
                 fixed
                     .into_iter()
@@ -122,8 +117,8 @@ impl SimplePeriodicContinuedFraction {
     }
 }
 
-impl Iterator for SimplePeriodicContinuedFraction {
-    type Item = Ratio<BigInt>;
+impl<T: Clone + Integer + CheckedAdd + CheckedMul> Iterator for SimpleContinuedFraction<T> {
+    type Item = Ratio<T>;
 
     fn next(&mut self) -> Option<Self::Item> {
         let out = Ratio::new(self.a1.clone(), self.b1.clone());
--- src/playground.rs
+++ src/playground.rs
@@ -2,7 +2,7 @@
 use crate::{
     core::{
         AbsDiffs, Boustrophedon, BoustrophedonTriangle, Constant, Exponential, Natural,
-        PartialSums, Prime, Ratios, SimpleContinuedFraction, SimplePeriodicContinuedFraction,
+        PartialSums, Prime, Ratios, SimpleContinuedFraction,
     },
     fibonacci::Fibonacci,
     figurate::Triangular,
@@ -24,7 +24,7 @@ crate::print_values!(
     Zeta::new(3), 0, 9; // converges on Apery's constant 1.202569...
     SimpleContinuedFraction::new(Constant::new(1)), 0, 10; // converges on golden ratio 1.61803398875...
     SimpleContinuedFraction::new(Constant::new(2)), 0, 10; // converges on silver ratio 2.414213562...
-    SimplePeriodicContinuedFraction::new(crate::big!(vec![1]), crate::big!(vec![2])), 0, 10; // converges on sqrt(2) 1.41421356237...
+    SimpleContinuedFraction::new_periodic(crate::big!(vec![1]), crate::big!(vec![2])), 0, 10; // converges on sqrt(2) 1.41421356237...
 );
 
 crate::print_rows!(
