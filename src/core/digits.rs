7129da85e1db0851a8c4a320000036715002a434
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-09-26 19:02:25 -0400

    digits

--- src/core/digits.rs
+++ src/core/digits.rs
@@ -19,9 +19,9 @@ pub fn digital_root<N: Integer + PrimInt>(mut n: N, base: N) -> N {
     n
 }
 
-pub fn digital_sum_big(mut n: BigInt, base: &BigInt) -> BigInt {
+pub fn digital_sum_big(mut n: BigInt, base: u64) -> BigInt {
     let mut total = BigInt::zero();
-
+    let base = BigInt::from(base);
     while n.is_positive() {
         let (div, rem) = n.div_rem(&base);
         total = total + rem.to_u64().unwrap(); // always works because base is limited to u64
@@ -31,78 +31,80 @@ pub fn digital_sum_big(mut n: BigInt, base: &BigInt) -> BigInt {
     total
 }
 
-pub fn digital_root_big(mut n: BigInt, base: &BigInt) -> BigInt {
-    while n >= *base {
+pub fn digital_root_big(mut n: BigInt, base: u64) -> BigInt {
+    while n >= base.into() {
         n = digital_sum_big(n, base)
     }
     n
 }
 
+/// The sums of the digits of each natural number.
 pub struct DigitalSums<N> {
     ctr: N,
     base: N,
 }
 
-impl DigitalSums<BigInt> {
-    pub fn new_big(base: u64) -> Self {
-        assert!(base >= 2);
+impl<N: PrimInt + Integer> DigitalSums<N> {
+    pub fn new(base: N) -> Self {
+        assert!(base >= N::from(2).unwrap());
         Self {
-            ctr: BigInt::zero(),
-            base: BigInt::from(base),
+            ctr: N::zero(),
+            base: base,
         }
     }
 }
 
-impl Iterator for DigitalSums<BigInt> {
-    type Item = BigInt;
+impl<N: PrimInt + Integer> Iterator for DigitalSums<N> {
+    type Item = N;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let out = digital_sum_big(self.ctr.clone(), &self.base);
-        self.ctr += 1;
+        let out = digital_sum(self.ctr.clone(), self.base);
+        self.ctr = self.ctr.checked_add(&N::one())?;
         Some(out)
     }
 
     fn nth(&mut self, n: usize) -> Option<Self::Item> {
-        self.ctr += n;
-        let out = digital_sum_big(self.ctr.clone(), &self.base);
-        self.ctr += 1;
+        self.ctr = self.ctr.checked_add(&N::from(n)?)?;
+        let out = digital_sum(self.ctr.clone(), self.base);
+        self.ctr = self.ctr.checked_add(&N::one())?;
         Some(out)
     }
 }
 
+/// The digital root of each natural number.
 pub struct DigitalRoots<N> {
     ctr: N,
     base: N,
 }
 
-impl DigitalRoots<BigInt> {
-    pub fn new_big(base: u64) -> Self {
-        assert!(base >= 2);
+impl<N: PrimInt> DigitalRoots<N> {
+    pub fn new(base: N) -> Self {
+        assert!(base >= N::from(2).unwrap());
         Self {
-            ctr: BigInt::zero(),
-            base: BigInt::from(base),
+            ctr: N::zero(),
+            base: base,
         }
     }
 }
 
-impl Iterator for DigitalRoots<BigInt> {
-    type Item = BigInt;
+impl<N: PrimInt + Integer> Iterator for DigitalRoots<N> {
+    type Item = N;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let out = digital_root_big(self.ctr.clone(), &self.base);
-        self.ctr += 1;
+        let out = digital_root(self.ctr.clone(), self.base);
+        self.ctr = self.ctr.checked_add(&N::one())?;
         Some(out)
     }
 
     fn nth(&mut self, n: usize) -> Option<Self::Item> {
-        self.ctr += n;
-        let out = digital_root_big(self.ctr.clone(), &self.base);
-        self.ctr += 1;
+        self.ctr = self.ctr.checked_add(&N::from(n)?)?;
+        let out = digital_root(self.ctr.clone(), self.base);
+        self.ctr = self.ctr.checked_add(&N::one())?;
         Some(out)
     }
 }
 
 crate::check_sequences!(
-    DigitalSums::new_big(10), 123, 10, [6, 7, 8, 9, 10, 11, 12, 4, 5, 6];
-    DigitalRoots::new_big(10), 123, 10, [6, 7, 8, 9, 1, 2, 3, 4, 5, 6];
+    DigitalSums::new(10), 1234, 20,  [10, 11, 12, 13, 14, 15, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 8, 9, 10, 11];
+    DigitalRoots::new(10), 1234, 20, [1,  2,  3,  4,  5,  6,  7, 8, 9,  1,  2,  3,  4,  5,  6,  7, 8, 9,  1,  2];
 );
