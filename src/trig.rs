de40b478162913e71b4f7ff2b5bced10ebf33ae2
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-09-28 10:02:36 -0400

    more trig functions

--- src/trig.rs
+++ src/trig.rs
@@ -1,9 +1,8 @@
+use crate::core::Factorials;
 use num::{BigInt, One, Zero, rational::Ratio};
 
-use crate::core::Factorials;
-
 /// Partial sums of the Taylor series expansion of the sine function evaluated at n/d.
-pub struct Sin {
+pub struct Sine {
     sum: Ratio<BigInt>,
     val: Ratio<BigInt>,
     x: Ratio<BigInt>,
@@ -11,7 +10,7 @@ pub struct Sin {
     neg: bool,
 }
 
-impl Sin {
+impl Sine {
     pub fn new_big<T>(n: T, d: T) -> Self
     where
         BigInt: From<T>,
@@ -27,7 +26,7 @@ impl Sin {
     }
 }
 
-impl Iterator for Sin {
+impl Iterator for Sine {
     type Item = Ratio<BigInt>;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -46,15 +45,60 @@ impl Iterator for Sin {
     }
 }
 
-/// Partial sums of the Taylor series expansion of the hyperbolic sine function evaluated at n/d.
-pub struct SinH {
+/// Partial sums of the Taylor series expansion of the cosine function evaluated at n/d.
+pub struct Cosine {
     sum: Ratio<BigInt>,
     val: Ratio<BigInt>,
     x: Ratio<BigInt>,
     factorials: Factorials<BigInt>,
+    neg: bool,
 }
 
-impl SinH {
+impl Cosine {
+    pub fn new_big<T>(n: T, d: T) -> Self
+    where
+        BigInt: From<T>,
+    {
+        let x = Ratio::new(BigInt::from(n), BigInt::from(d));
+        Self {
+            sum: Ratio::zero(),
+            val: Ratio::one(),
+            x,
+            factorials: Factorials::new_big(),
+            neg: true,
+        }
+    }
+}
+
+impl Iterator for Cosine {
+    type Item = Ratio<BigInt>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let out = self.sum.clone();
+        let frac = Ratio::new(BigInt::one(), self.factorials.next()?);
+        self.factorials.next(); // discard one factorial
+        if self.neg {
+            self.sum += frac * &self.val;
+        } else {
+            self.sum -= frac * &self.val;
+        }
+        self.val *= &self.x;
+        self.val *= &self.x;
+        self.neg = !self.neg;
+        Some(out)
+    }
+}
+
+/// Partial sums of the Taylor series expansion of the cosecant = 1/sin(x) function evaluated at n/d.
+pub struct Cosecant {
+    sum: Ratio<BigInt>,
+    val: Ratio<BigInt>,
+    x: Ratio<BigInt>,
+    factorials: Factorials<BigInt>,
+    neg: bool,
+}
+
+impl Cosecant {
     pub fn new_big<T>(n: T, d: T) -> Self
     where
         BigInt: From<T>,
@@ -65,20 +109,78 @@ impl SinH {
             val: x.clone(),
             x,
             factorials: Factorials::new_big(),
+            neg: true,
         }
     }
 }
 
-impl Iterator for SinH {
+impl Iterator for Cosecant {
     type Item = Ratio<BigInt>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let out = self.sum.clone();
+        let out = if self.sum.is_zero() {
+            self.sum.clone()
+        } else {
+            self.sum.recip().clone()
+        };
         self.factorials.next(); // discard one factorial
         let frac = Ratio::new(BigInt::one(), self.factorials.next()?);
-        self.sum += frac * &self.val;
+        if self.neg {
+            self.sum += frac * &self.val;
+        } else {
+            self.sum -= frac * &self.val;
+        }
         self.val *= &self.x;
         self.val *= &self.x;
+        self.neg = !self.neg;
+        Some(out)
+    }
+}
+
+/// Partial sums of the Taylor series expansion of the secant = 1/cos(x) function evaluated at n/d.
+pub struct Secant {
+    sum: Ratio<BigInt>,
+    val: Ratio<BigInt>,
+    x: Ratio<BigInt>,
+    factorials: Factorials<BigInt>,
+    neg: bool,
+}
+
+impl Secant {
+    pub fn new_big<T>(n: T, d: T) -> Self
+    where
+        BigInt: From<T>,
+    {
+        let x = Ratio::new(BigInt::from(n), BigInt::from(d));
+        Self {
+            sum: Ratio::zero(),
+            val: Ratio::one(),
+            x,
+            factorials: Factorials::new_big(),
+            neg: true,
+        }
+    }
+}
+
+impl Iterator for Secant {
+    type Item = Ratio<BigInt>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let out = if self.sum.is_zero() {
+            self.sum.clone()
+        } else {
+            self.sum.recip().clone()
+        };
+        let frac = Ratio::new(BigInt::one(), self.factorials.next()?);
+        self.factorials.next(); // discard one factorial
+        if self.neg {
+            self.sum += frac * &self.val;
+        } else {
+            self.sum -= frac * &self.val;
+        }
+        self.val *= &self.x;
+        self.val *= &self.x;
+        self.neg = !self.neg;
         Some(out)
     }
 }
@@ -87,7 +189,10 @@ impl Iterator for SinH {
 use crate::core::rational_digits::DecimalDigits;
 
 crate::check_sequences!(
-    DecimalDigits::from_ratio(Sin::new_big(2,1).skip(8).next().unwrap()), 0, 10, [0,9,0,9,2,9,7,4,2,6];
-    DecimalDigits::from_ratio(Sin::new_big(1,3).skip(8).next().unwrap()), 0, 10, [0,3,2,7,1,9,4,6,9,6];
-    DecimalDigits::from_ratio(SinH::new_big(1,1).skip(8).next().unwrap()), 0, 10, [1,1,7,5,2,0,1,1,9,3];
+    DecimalDigits::from_ratio_big(Sine::new_big(2,1).skip(10).next().unwrap()), 0, 10, [0, 9, 0, 9, 2, 9, 7, 4, 2, 6];
+    DecimalDigits::from_ratio_big(Sine::new_big(1,3).skip(10).next().unwrap()), 0, 10, [0, 3, 2, 7, 1, 9, 4, 6, 9, 6];
+    DecimalDigits::from_ratio_big(Cosine::new_big(1,3).skip(10).next().unwrap()), 0, 10, [0, 9, 4, 4, 9, 5, 6, 9, 4, 6];
+    DecimalDigits::from_ratio_big(Cosine::new_big(2,1).skip(10).next().unwrap()), 0, 10, [0, 4, 1, 6, 1, 4, 6, 8, 3, 6];
+    DecimalDigits::from_ratio_big(Secant::new_big(1,3).skip(10).next().unwrap()), 0, 10, [1, 0, 5, 8, 2, 4, 9, 2, 7, 1];
+    DecimalDigits::from_ratio_big(Cosecant::new_big(1,3).skip(10).next().unwrap()), 0, 10, [3, 0, 5, 6, 2, 8, 4, 2, 5, 4];
 );
