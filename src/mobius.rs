fdec2d03163f5ea2c302a371193262e10fcaec64
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-10-17 15:27:45 -0400

    mertens

--- src/mobius.rs
+++ src/mobius.rs
@@ -1,8 +1,6 @@
-use std::marker::PhantomData;
-
-use num::{BigInt, Integer, One, Signed, Zero};
-
 use crate::core::primality_utils::prime_factorization;
+use num::{BigInt, CheckedAdd, Integer, One, Signed, Zero};
+use std::marker::PhantomData;
 
 /// The Möbius function over the positive integers
 pub struct Mobius<T> {
@@ -51,6 +49,56 @@ impl<T: One + Zero + Signed> Iterator for Mobius<T> {
     }
 }
 
+/// The values of the Merterns function over the positive integers. Partial sums of the Mobius function, without the leading zero.
+pub struct Mertens<T> {
+    n: u64,
+    sum: T,
+}
+
+impl<T: One + Zero + Signed> Mertens<T> {
+    pub fn new() -> Self {
+        Self {
+            n: 0,
+            sum: T::zero(),
+        }
+    }
+}
+
+impl Mertens<BigInt> {
+    pub fn new_big() -> Self {
+        Self {
+            n: 0,
+            sum: BigInt::zero(),
+        }
+    }
+}
+
+impl<T: One + Zero + Signed + CheckedAdd + Clone> Iterator for Mertens<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.n = self.n.checked_add(1)?;
+
+        if self.n == 1 {
+            self.sum = self.sum.checked_add(&T::one())?;
+        } else {
+            let pf = prime_factorization(self.n);
+            if pf.iter().map(|x| x.1).any(|m| m > 1) {
+                // add zero
+            } else {
+                if pf.len().is_even() {
+                    self.sum = self.sum.checked_add(&T::one())?;
+                } else {
+                    self.sum = self.sum.checked_add(&-T::one())?;
+                }
+            }
+        }
+
+        Some(self.sum.clone())
+    }
+}
+
 crate::check_sequences!(
     Mobius::<i32>::new(), 0, 20, [1, -1, -1, 0, -1, 1, -1, 0, 0, 1, -1, 0, -1, 1, 1, 0, -1, 0, -1, 0];
+    Mertens::<i32>::new(), 0, 20, [1, 0, -1, -1, -2, -1, -2, -2, -2, -1, -2, -2, -3, -2, -1, -1, -2, -2, -3, -3];
 );
