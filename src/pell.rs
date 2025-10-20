c42a0229a6f872066fec52ba59770ade76ff3680
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-10-20 08:45:29 -0400

    brought pell to current standard

--- src/pell.rs
+++ src/pell.rs
@@ -1,42 +1,14 @@
-use num::{BigInt, PrimInt};
+use num::{BigInt, CheckedAdd, One, PrimInt, Zero};
 
 /// The Pell numbers.
 /// 0, 1, 2, 5, 12, 29, 70, 169, 408, 985...
-pub struct Pell {
-    a: BigInt,
-    b: BigInt,
-}
-
-impl Pell {
-    pub fn new_big() -> Self {
-        Self {
-            a: BigInt::from(0),
-            b: BigInt::from(1),
-        }
-    }
-}
-
-impl Iterator for Pell {
-    type Item = BigInt;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let out = self.a.clone();
-        let t = &self.a + (&self.b * 2);
-        self.a = self.b.clone();
-        self.b = t;
-        Some(out)
-    }
-}
-
-/// The Pell numbers.
-/// 0, 1, 2, 5, 12, 29, 70, 169, 408, 985...
-pub struct PellGeneric<T> {
+pub struct Pell<T> {
     a: T,
     b: T,
 }
 
-impl<T: PrimInt> PellGeneric<T> {
-    pub fn new_big() -> Self {
+impl<T: PrimInt> Pell<T> {
+    pub fn new() -> Self {
         Self {
             a: T::zero(),
             b: T::one(),
@@ -44,41 +16,21 @@ impl<T: PrimInt> PellGeneric<T> {
     }
 }
 
-impl<T: PrimInt> Iterator for PellGeneric<T> {
-    type Item = T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let out = self.a.clone();
-        let two = T::one() + T::one();
-        let t = self.a.checked_add(&(self.b.checked_mul(&two)?))?;
-        self.a = self.b.clone();
-        self.b = t;
-        Some(out)
-    }
-}
-
-/// The companion Pell numbers.
-/// 2, 2, 6, 14, 34, 82, 198, 478, 1154, 2786...
-pub struct CompanionPell {
-    a: BigInt,
-    b: BigInt,
-}
-
-impl CompanionPell {
+impl Pell<BigInt> {
     pub fn new_big() -> Self {
         Self {
-            a: BigInt::from(2),
-            b: BigInt::from(2),
+            a: BigInt::zero(),
+            b: BigInt::one(),
         }
     }
 }
 
-impl Iterator for CompanionPell {
-    type Item = BigInt;
+impl<T: One + Zero + CheckedAdd + Clone> Iterator for Pell<T> {
+    type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
         let out = self.a.clone();
-        let t = &self.a + (&self.b * 2);
+        let t = self.a.checked_add(&(self.b.checked_add(&self.b)?))?;
         self.a = self.b.clone();
         self.b = t;
         Some(out)
@@ -87,13 +39,13 @@ impl Iterator for CompanionPell {
 
 /// The companion Pell numbers.
 /// 2, 2, 6, 14, 34, 82, 198, 478, 1154, 2786...
-pub struct CompanionPellGeneric<T> {
+pub struct CompanionPell<T> {
     a: T,
     b: T,
 }
 
-impl<T: PrimInt> CompanionPellGeneric<T> {
-    pub fn new_big() -> Self {
+impl<T: PrimInt> CompanionPell<T> {
+    pub fn new() -> Self {
         Self {
             a: T::one() + T::one(),
             b: T::one() + T::one(),
@@ -101,13 +53,21 @@ impl<T: PrimInt> CompanionPellGeneric<T> {
     }
 }
 
-impl<T: PrimInt> Iterator for CompanionPellGeneric<T> {
+impl CompanionPell<BigInt> {
+    pub fn new_big() -> Self {
+        Self {
+            a: BigInt::one() + BigInt::one(),
+            b: BigInt::one() + BigInt::one(),
+        }
+    }
+}
+
+impl<T: One + Zero + CheckedAdd + Clone> Iterator for CompanionPell<T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
         let out = self.a.clone();
-        let two = T::one() + T::one();
-        let t = self.a.checked_add(&(self.b.checked_mul(&two)?))?;
+        let t = self.a.checked_add(&(self.b.checked_add(&self.b)?))?;
         self.a = self.b.clone();
         self.b = t;
         Some(out)
@@ -120,7 +80,5 @@ crate::check_iteration_times!(
 
 crate::check_sequences!(
     Pell::new_big(), 0, 10, [0, 1, 2, 5, 12, 29, 70, 169, 408, 985];
-    // PellGeneric::<u64>::new(), 0, 10, [0, 1, 2, 5, 12, 29, 70, 169, 408, 985];
     CompanionPell::new_big(), 0, 10, [2, 2, 6, 14, 34, 82, 198, 478, 1154, 2786];
-    // CompanionPellGeneric::<u64>::new(), 0, 10, [2, 2, 6, 14, 34, 82, 198, 478, 1154, 2786];
 );
