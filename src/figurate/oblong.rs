65d98056bb4cfc471d2170b46195f6b26b18d051
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-11-16 11:48:27 -0500

    Update oblong.rs

--- src/figurate/oblong.rs
+++ src/figurate/oblong.rs
@@ -1,13 +1,22 @@
-use num::BigInt;
+use num::{BigInt, CheckedAdd, One, Zero};
 
 /// The oblong numbers.
 /// 0, 2, 6, 12, 20, 30, 42, 56, 72, 90...
-pub struct Oblong {
-    val: BigInt,
-    ctr: BigInt,
+pub struct Oblong<T> {
+    val: T,
+    ctr: T,
 }
 
-impl Oblong {
+impl<T: CheckedAdd + Clone + One + Zero> Oblong<T> {
+    pub fn new() -> Self {
+        Self {
+            val: T::zero(),
+            ctr: T::one() + T::one(),
+        }
+    }
+}
+
+impl Oblong<BigInt> {
     pub fn new_big() -> Self {
         Self {
             val: BigInt::from(0),
@@ -24,13 +33,13 @@ impl Oblong {
     }
 }
 
-impl Iterator for Oblong {
-    type Item = BigInt;
+impl<T: CheckedAdd + Clone + One> Iterator for Oblong<T> {
+    type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
         let out = self.val.clone();
-        self.val += &self.ctr;
-        self.ctr += 2;
+        self.val = self.val.checked_add(&self.ctr)?;
+        self.ctr = self.ctr.checked_add(&(T::one() + T::one()))?;
         Some(out)
     }
 }
@@ -41,4 +50,5 @@ crate::check_iteration_times!(
 
 crate::check_sequences!(
     Oblong::new_big(), 0, 10, [0, 2, 6, 12, 20, 30, 42, 56, 72, 90];
+    Oblong::<i8>::new(), 0, 10, [0, 2, 6, 12, 20, 30, 42, 56, 72, 90];
 );
