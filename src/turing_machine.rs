cfcb872f205c8ae8af9eac93e1fbdc480987ff4e
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-11-19 09:37:53 -0500

    working turing machine

--- src/turing_machine.rs
+++ src/turing_machine.rs
@@ -1,4 +1,9 @@
-use std::collections::{HashMap, VecDeque};
+use std::{
+    collections::{HashMap, VecDeque},
+    fmt::Display,
+};
+
+use itertools::Itertools;
 
 pub enum Move {
     Left,
@@ -6,20 +11,7 @@ pub enum Move {
     Stay,
 }
 
-#[macro_export]
-macro_rules! turing_state {
-    ($name:ident; $($a:literal => $symbol:literal, $movement:expr, $state:literal);+ $(;)?) => {
-        fn $name(x: char) -> (char, Move, &'static str) {
-            match x {
-                $(
-                    $a => ($symbol, $movement, $state),
-                )+
-                _ => panic!("symbol not handled"),
-            }
-        }
-    };
-}
-
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct TuringTape {
     tape: VecDeque<char>,
     position: usize,
@@ -27,6 +19,30 @@ pub struct TuringTape {
 }
 
 impl TuringTape {
+    pub fn new(tape: Vec<char>, position: usize, blank: char) -> Self {
+        if tape.is_empty() {
+            Self {
+                tape: VecDeque::from([blank]),
+                position,
+                blank,
+            }
+        } else {
+            Self {
+                tape: VecDeque::from(tape),
+                position,
+                blank,
+            }
+        }
+    }
+
+    pub fn read(&self) -> char {
+        self.tape[self.position]
+    }
+
+    pub fn write(&mut self, symbol: char) {
+        self.tape[self.position] = symbol;
+    }
+
     pub fn shift(&mut self, direction: Move) {
         match direction {
             Move::Left => {
@@ -51,36 +67,125 @@ impl TuringTape {
     }
 }
 
+impl Display for TuringTape {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut s = " ".repeat(self.position);
+        s.push('.');
+        s.push('\n');
+        s.push_str(&self.tape.iter().join(""));
+        write!(f, "{}", s)
+    }
+}
+
 pub struct TuringMachine {
     tape: TuringTape,
-    position: usize,
     states: HashMap<&'static str, Box<dyn Fn(char) -> (char, Move, &'static str)>>,
     current_state: &'static str,
 }
 
 impl TuringMachine {
-    pub fn new() -> Self {
-        todo!()
+    /// A new TuringMachine with a tape that has the
+    pub fn new(
+        tape: Vec<char>,
+        blank: char,
+        states: Vec<Box<dyn Fn(char) -> (char, Move, &'static str)>>,
+        state_names: Vec<&'static str>,
+    ) -> Self {
+        if state_names.contains(&"HALT") {
+            panic!("the HALT state is handled specially")
+        }
+        Self {
+            tape: TuringTape::new(tape, 0, blank),
+            states: HashMap::from_iter(state_names.iter().cloned().zip(states.into_iter())),
+            current_state: state_names[0],
+        }
+    }
+}
+
+impl Iterator for TuringMachine {
+    type Item = TuringTape;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let out = self.tape.clone();
+
+        if self.current_state == "HALT" {
+            return None;
+        }
+
+        let cur_symbol = self.tape.read();
+        let (symbol, direction, next_state) = self.states[self.current_state](cur_symbol);
+        self.tape.write(symbol);
+        self.tape.shift(direction);
+        self.current_state = next_state;
+
+        Some(out)
+    }
+}
+
+#[macro_export]
+macro_rules! turing_state {
+    ($name:ident; $($a:literal => $symbol:literal, $movement:expr, $state:literal);+ $(;)?) => {
+        fn $name(x: char) -> (char, Move, &'static str) {
+            match x {
+                $(
+                    $a => ($symbol, $movement, $state),
+                )+
+                _ => panic!("symbol not handled"),
+            }
+        }
+    };
+}
+
+#[cfg(test)]
+#[ignore = "visualization"]
+#[test]
+fn test_tape() {
+    let mut tape = TuringTape::new(vec!['1', '1', '1'], 0, '0');
+    println!("{}", tape);
+    for i in [
+        Move::Left,
+        Move::Left,
+        Move::Right,
+        Move::Right,
+        Move::Right,
+        Move::Right,
+        Move::Right,
+        Move::Right,
+    ] {
+        tape.shift(i);
+        println!("{}", tape);
     }
 }
 
 #[cfg(test)]
-turing_state!(
-    state0;
-    '0' => '1', Move::Right, "B";
-    '1' => '1', Move::Left, "C";
-);
+#[ignore = "visualization"]
+#[test]
+fn busy_beaver() {
+    turing_state!(
+        state0;
+        '0' => '1', Move::Right, "B";
+        '1' => '1', Move::Left, "C";
+    );
 
-#[cfg(test)]
-turing_state!(
-    state1;
-    '0' => '1', Move::Left, "A";
-    '1' => '1', Move::Right, "B";
-);
+    turing_state!(
+        state1;
+        '0' => '1', Move::Left, "A";
+        '1' => '1', Move::Right, "B";
+    );
 
-#[cfg(test)]
-turing_state!(
-    state2;
-    '0' => '1', Move::Left, "B";
-    '1' => '1', Move::Right, "B";
-);
+    turing_state!(
+        state2;
+        '0' => '1', Move::Left, "B";
+        '1' => '1', Move::Right, "HALT";
+    );
+
+    let machine = TuringMachine::new(
+        vec!['0'],
+        '0',
+        vec![Box::new(state0), Box::new(state1), Box::new(state2)],
+        vec!["A", "B", "C"],
+    );
+    for tape in machine {
+        println!("{}", tape);
+    }
+}
