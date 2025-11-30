403f35a0652796ebdfc2b11fc14c7f4ea8e4a874
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-11-30 18:34:30 -0500

    simpler automorphic

--- src/automorphic.rs
+++ src/automorphic.rs
@@ -1,24 +1,31 @@
-use num::BigInt;
-
-const BASE: u32 = 10;
+use num::{BigInt, FromPrimitive, One};
 
 pub struct Automorphic {
     value: BigInt,
     digits: u32,
+    base: u32,
 }
 
 impl Automorphic {
-    pub fn new_big_5() -> Self {
+    pub fn new_big<T>(value: T, base: u32) -> Self
+    where
+        BigInt: From<T>,
+    {
+        let value = BigInt::from(value);
+        assert!(base >= 2);
+        assert!(
+            value < BigInt::from_u32(base).unwrap(),
+            "the starting value must be less than the base"
+        );
+        assert_eq!(
+            value,
+            (&value * &value) % BigInt::from_u32(base).unwrap(),
+            "this value cannot be the start of an automorphic number in this base"
+        );
         Self {
-            value: BigInt::from(5),
+            value,
             digits: 1,
-        }
-    }
-
-    pub fn new_big_6() -> Self {
-        Self {
-            value: BigInt::from(6),
-            digits: 1,
+            base,
         }
     }
 }
@@ -32,8 +39,10 @@ impl Iterator for Automorphic {
         loop {
             self.digits += 1;
             for digit in 1..=9 {
-                let test: BigInt = &self.value + BigInt::from(BASE).pow(self.digits - 1) * digit;
-                if (&test * &test) % BigInt::from(BASE).pow(self.digits) == test && test != out {
+                let test: BigInt =
+                    &self.value + BigInt::from(self.base).pow(self.digits - 1) * digit;
+                if (&test * &test) % BigInt::from(self.base).pow(self.digits) == test && test != out
+                {
                     self.value = test.clone();
                     return Some(out);
                 }
@@ -42,23 +51,36 @@ impl Iterator for Automorphic {
     }
 }
 
+/// The sequence of digits that define a k-adic automorphic number, if they exist. That is a number which when squared has all the same digits.
 pub struct AutomorphicDigits {
     value: BigInt,
     digits: u32,
+    base: BigInt,
 }
 
 impl AutomorphicDigits {
-    pub fn new_big_5() -> Self {
+    /// Panics if value is greater than the base.
+    /// Panics if the value is not a product of factors of the base.
+    pub fn new_big<T>(value: T, base: T) -> Self
+    where
+        BigInt: From<T>,
+    {
+        let value = BigInt::from(value);
+        let base = BigInt::from(base);
+        assert!(base >= BigInt::from_i32(2).unwrap());
+        assert!(
+            &value < &base,
+            "the starting value must be less than the base"
+        );
+        assert_eq!(
+            value,
+            (&value * &value) % &base,
+            "this value cannot be the start of an automorphic number in this base"
+        );
         Self {
-            value: BigInt::from(5),
+            value,
             digits: 1,
-        }
-    }
-
-    pub fn new_big_6() -> Self {
-        Self {
-            value: BigInt::from(6),
-            digits: 1,
+            base,
         }
     }
 }
@@ -67,12 +89,13 @@ impl Iterator for AutomorphicDigits {
     type Item = BigInt;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let out = self.value.clone() / BigInt::from(BASE).pow(self.digits - 1);
+        let out = self.value.clone() / self.base.pow(self.digits - 1);
 
         self.digits += 1;
-        for digit in 1..=9 {
-            let test: BigInt = &self.value + BigInt::from(BASE).pow(self.digits - 1) * digit;
-            if (&test * &test) % BigInt::from(BASE).pow(self.digits) == test && test != out {
+        for digit in num::iter::range_inclusive(BigInt::one(), self.base.clone()) {
+            // no need to test 0
+            let test: BigInt = &self.value + self.base.pow(self.digits - 1) * digit;
+            if (&test * &test) % self.base.pow(self.digits) == test && test != out {
                 self.value = test.clone();
             }
         }
@@ -82,8 +105,9 @@ impl Iterator for AutomorphicDigits {
 }
 
 crate::check_sequences!(
-    Automorphic::new_big_5(), 0, 8, [5, 25, 625, 90625, 890625, 2890625, 12890625, 212890625];
-    Automorphic::new_big_6(), 0, 8, [6, 76, 376, 9376, 109376, 7109376, 87109376, 787109376];
-    AutomorphicDigits::new_big_5(), 0, 10, [5, 2, 6, 0, 9, 8, 2, 1, 2, 8];
-    AutomorphicDigits::new_big_6(), 0, 10, [6, 7, 3, 9, 0, 1, 7, 8, 7, 1];
+    Automorphic::new_big(5, 10), 0, 8, [5, 25, 625, 90625, 890625, 2890625, 12890625, 212890625];
+    Automorphic::new_big(6, 10), 0, 8, [6, 76, 376, 9376, 109376, 7109376, 87109376, 787109376];
+    AutomorphicDigits::new_big(5, 10), 0, 10, [5, 2, 6, 0, 9, 8, 2, 1, 2, 8];
+    AutomorphicDigits::new_big(6, 10), 0, 10, [6, 7, 3, 9, 0, 1, 7, 8, 7, 1];
+    AutomorphicDigits::new_big(3, 6), 0, 10, [3, 1, 2, 0, 5, 3, 1, 2, 2, 2];
 );
