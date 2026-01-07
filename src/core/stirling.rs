83c890812c8272703ef7d15a1e2770980640842c
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-01-07 15:10:25 -0500

    Update stirling.rs

--- src/core/stirling.rs
+++ src/core/stirling.rs
@@ -1,4 +1,4 @@
-use num::{BigInt, One, Zero};
+use num::{BigInt, CheckedAdd, CheckedMul, One, Zero};
 
 use crate::check_sequences;
 
@@ -14,13 +14,22 @@ pub fn unsigned_stirling_first(n: &BigInt, k: &BigInt) -> BigInt {
 }
 
 /// Unsigned Stirling numbers of the first kind by rows.
-pub struct UnsignedStirlingFirst {
-    n: BigInt,
-    row: Vec<BigInt>,
+pub struct UnsignedStirlingFirst<T> {
+    n: T,
+    row: Vec<T>,
 }
 
-impl UnsignedStirlingFirst {
+impl<T: One + Zero + CheckedAdd + CheckedMul + Clone> UnsignedStirlingFirst<T> {
     pub fn new() -> Self {
+        Self {
+            n: T::zero(),
+            row: vec![T::one()],
+        }
+    }
+}
+
+impl UnsignedStirlingFirst<BigInt> {
+    pub fn new_big() -> Self {
         Self {
             n: BigInt::zero(),
             row: vec![BigInt::one()],
@@ -28,20 +37,24 @@ impl UnsignedStirlingFirst {
     }
 }
 
-impl Iterator for UnsignedStirlingFirst {
-    type Item = Vec<BigInt>;
+impl<T: One + Zero + CheckedAdd + CheckedMul + Clone> Iterator for UnsignedStirlingFirst<T> {
+    type Item = Vec<T>;
 
     fn next(&mut self) -> Option<Self::Item> {
         let out = Some(self.row.clone());
         let mut next_row = Vec::with_capacity(self.row.len() + 1);
-        next_row.push(BigInt::zero()); // every row except the first starts with zero
+        next_row.push(T::zero()); // every row except the first starts with zero
 
         for k in 1..self.row.len() {
-            next_row.push(&self.n * &self.row[k] + &self.row[k - 1]);
+            next_row.push(
+                self.n
+                    .checked_mul(&self.row[k])?
+                    .checked_add(&self.row[k - 1])?,
+            );
         }
-        next_row.push(BigInt::one()); // every row ends with one
+        next_row.push(T::one()); // every row ends with one
 
-        self.n += 1;
+        self.n = self.n.checked_add(&T::one())?;
         self.row = next_row;
 
         out
@@ -51,5 +64,5 @@ impl Iterator for UnsignedStirlingFirst {
 pub struct StirlingSecond {}
 
 check_sequences!(
-    UnsignedStirlingFirst::new().flatten(), 0, 20, [1, 0, 1, 0, 1, 1, 0, 2, 3, 1, 0, 6, 11, 6, 1, 0, 24, 50, 35, 10];
+    UnsignedStirlingFirst::new_big().flatten(), 0, 20, [1, 0, 1, 0, 1, 1, 0, 2, 3, 1, 0, 6, 11, 6, 1, 0, 24, 50, 35, 10];
 );
