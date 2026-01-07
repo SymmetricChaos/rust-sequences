825e18ba755ca818ebad1495a3b6f8f4e9e27e7e
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-01-07 11:57:19 -0500

    better stirling

--- src/core/stirling.rs
+++ src/core/stirling.rs
@@ -13,33 +13,37 @@ pub fn unsigned_stirling_first(n: &BigInt, k: &BigInt) -> BigInt {
     (n - 1) * unsigned_stirling_first(&(n - 1), &k) + unsigned_stirling_first(&(n - 1), &(k - 1))
 }
 
-/// Unsigned Stirling numbers of the first kind.
+/// Unsigned Stirling numbers of the first kind by rows.
 pub struct UnsignedStirlingFirst {
     n: BigInt,
-    k: BigInt,
+    row: Vec<BigInt>,
 }
 
 impl UnsignedStirlingFirst {
     pub fn new() -> Self {
         Self {
             n: BigInt::zero(),
-            k: BigInt::zero(),
+            row: vec![BigInt::one()],
         }
     }
 }
 
 impl Iterator for UnsignedStirlingFirst {
-    type Item = BigInt;
+    type Item = Vec<BigInt>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let out = Some(unsigned_stirling_first(&self.n, &self.k));
-        println!("{} {} = {} ", self.n, self.k, out.clone().unwrap());
-        if self.n == self.k {
-            self.n += 1;
-            self.k = BigInt::zero();
-        } else {
-            self.k += 1;
+        let out = Some(self.row.clone());
+        let mut next_row = Vec::with_capacity(self.row.len() + 1);
+        next_row.push(BigInt::zero()); // every row except the first starts with zero
+
+        for k in 1..self.row.len() {
+            next_row.push(&self.n * &self.row[k] + &self.row[k - 1]);
         }
+        next_row.push(BigInt::one()); // every row ends with one
+
+        self.n += 1;
+        self.row = next_row;
+
         out
     }
 }
@@ -47,5 +51,5 @@ impl Iterator for UnsignedStirlingFirst {
 pub struct StirlingSecond {}
 
 check_sequences!(
-    UnsignedStirlingFirst::new(), 0, 20, [1, 0, 1, 0, 1, 1, 0, 2, 3, 1, 0, 6, 11, 6, 1, 0, 24, 50, 35, 10];
+    UnsignedStirlingFirst::new().flatten(), 0, 20, [1, 0, 1, 0, 1, 1, 0, 2, 3, 1, 0, 6, 11, 6, 1, 0, 24, 50, 35, 10];
 );
