bbb5015d00f6d03a57682e2278503a3071d78834
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-02-22 12:06:48 -0500

    better continued fraction api

--- src/core/continued_fraction.rs
+++ src/core/continued_fraction.rs
@@ -2,85 +2,85 @@ use num::{CheckedAdd, CheckedMul, Integer, rational::Ratio};
 
 use crate::print_sequences;
 
-/// Produce the convergents of a continued fraction given integer sequences representing the partial numerators and partial denominators.
-pub struct ContinuedFraction<T> {
-    a0: T,
-    b0: T,
-    a1: T,
-    b1: T,
-    numerators: Box<dyn Iterator<Item = T>>,
-    denominators: Box<dyn Iterator<Item = T>>,
-}
+// /// Produce the convergents of a continued fraction given integer sequences representing the partial numerators and partial denominators.
+// pub struct ContinuedFraction<T> {
+//     a0: T,
+//     b0: T,
+//     a1: T,
+//     b1: T,
+//     numerators: Box<dyn Iterator<Item = T>>,
+//     denominators: Box<dyn Iterator<Item = T>>,
+// }
 
-impl<T: CheckedAdd + CheckedMul + Clone + Integer + 'static> ContinuedFraction<T> {
-    pub fn new<N, D>(n: N, mut d: D) -> Self
-    where
-        N: Iterator<Item = T> + 'static,
-        D: Iterator<Item = T> + 'static,
-    {
-        Self {
-            a0: T::one(),
-            b0: T::zero(),
-            a1: d.next().unwrap(),
-            b1: T::one(),
-            numerators: Box::new(n),
-            denominators: Box::new(d),
-        }
-    }
+// impl<T: CheckedAdd + CheckedMul + Clone + Integer + 'static> ContinuedFraction<T> {
+//     pub fn new<N, D>(n: N, mut d: D) -> Self
+//     where
+//         N: Iterator<Item = T> + 'static,
+//         D: Iterator<Item = T> + 'static,
+//     {
+//         Self {
+//             a0: T::one(),
+//             b0: T::zero(),
+//             a1: d.next().unwrap(),
+//             b1: T::one(),
+//             numerators: Box::new(n),
+//             denominators: Box::new(d),
+//         }
+//     }
 
-    pub fn new_periodic(
-        fixed_n: Vec<T>,
-        fixed_d: Vec<T>,
-        periodic_n: Vec<T>,
-        periodic_d: Vec<T>,
-    ) -> Self {
-        Self {
-            a0: T::one(),
-            b0: T::one(),
-            a1: fixed_d[0].clone(),
-            b1: T::one(),
-            numerators: Box::new(
-                fixed_n
-                    .into_iter()
-                    .chain(periodic_n.into_iter().cycle())
-                    .skip(1),
-            ),
-            denominators: Box::new(
-                fixed_d
-                    .into_iter()
-                    .chain(periodic_d.into_iter().cycle())
-                    .skip(1),
-            ),
-        }
-    }
-}
+//     pub fn new_periodic(
+//         fixed_n: Vec<T>,
+//         fixed_d: Vec<T>,
+//         periodic_n: Vec<T>,
+//         periodic_d: Vec<T>,
+//     ) -> Self {
+//         Self {
+//             a0: T::one(),
+//             b0: T::one(),
+//             a1: fixed_d[0].clone(),
+//             b1: T::one(),
+//             numerators: Box::new(
+//                 fixed_n
+//                     .into_iter()
+//                     .chain(periodic_n.into_iter().cycle())
+//                     .skip(1),
+//             ),
+//             denominators: Box::new(
+//                 fixed_d
+//                     .into_iter()
+//                     .chain(periodic_d.into_iter().cycle())
+//                     .skip(1),
+//             ),
+//         }
+//     }
+// }
 
-impl<T: Clone + Integer + CheckedAdd + CheckedMul> Iterator for ContinuedFraction<T> {
-    type Item = Ratio<T>;
+// impl<T: Clone + Integer + CheckedAdd + CheckedMul + Display> Iterator for ContinuedFraction<T> {
+//     type Item = Ratio<T>;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        let out = Ratio::new(self.a1.clone(), self.b1.clone());
+//     fn next(&mut self) -> Option<Self::Item> {
+//         let out = Ratio::new(self.a1.clone(), self.b1.clone());
 
-        let n = self.numerators.next()?;
-        let d = self.denominators.next()?;
+//         let n = self.numerators.next()?;
+//         let d = self.denominators.next()?;
 
-        let a2 = d
-            .checked_mul(&self.a1)?
-            .checked_add(&n.checked_mul(&self.a0)?)?;
-        let b2 = d
-            .checked_mul(&self.b1)?
-            .checked_add(&n.checked_mul(&self.b0)?)?;
+//         let a2 = d
+//             .checked_mul(&self.a1)?
+//             .checked_add(&n.checked_mul(&self.a0)?)?;
+//         let b2 = d
+//             .checked_mul(&self.b1)?
+//             .checked_add(&n.checked_mul(&self.b0)?)?;
 
-        self.a0 = self.a1.clone();
-        self.b0 = self.b1.clone();
-        self.a1 = a2;
-        self.b1 = b2;
+//         self.a0 = self.a1.clone();
+//         self.b0 = self.b1.clone();
+//         self.a1 = a2;
+//         self.b1 = b2;
 
-        Some(out)
-    }
-}
+//         Some(out)
+//     }
+// }
 
-/// Produce the convergents of a simple continued fraction given an integer sequence representing the partial denominators.
+/// Produce the convergents of a simple continued fraction given an integer sequence representing the partial denominators. The numerators are always 1.
 pub struct SimpleContinuedFraction<T> {
     a0: T,
     b0: T,
@@ -103,18 +103,13 @@ impl<T: CheckedAdd + CheckedMul + Clone + Integer + 'static> SimpleContinuedFrac
         }
     }
 
-    pub fn new_periodic(fixed: Vec<T>, periodic: Vec<T>) -> Self {
+    pub fn new_periodic(fixed: T, periodic: &[T]) -> Self {
         Self {
             a0: T::one(),
             b0: T::zero(),
-            a1: fixed[0].clone(),
+            a1: fixed,
             b1: T::one(),
-            dens: Box::new(
-                fixed
-                    .into_iter()
-                    .chain(periodic.into_iter().cycle())
-                    .skip(1),
-            ),
+            dens: Box::new(periodic.to_vec().into_iter().cycle()),
         }
     }
 }
@@ -140,8 +135,8 @@ impl<T: Clone + Integer + CheckedAdd + CheckedMul> Iterator for SimpleContinuedF
 }
 
 #[cfg(test)]
-use crate::core::{constant::Constant, rational_digits::rational_decimal_string};
+use crate::core::rational_digits::rational_decimal_string;
 print_sequences!(
-    SimpleContinuedFraction::new(Constant::new(1)).map(|q| rational_decimal_string(q, 5).unwrap()), 0, 15; // Converges on phi
-    ContinuedFraction::new_periodic(vec![1],vec![1],vec![2],vec![2]), 0, 15; //
+    SimpleContinuedFraction::new_periodic(1, &[1]).map(|q| rational_decimal_string(q, 5).unwrap()), 0, 10; // Converges on phi
+    SimpleContinuedFraction::new_periodic(1, &[2]).map(|q| rational_decimal_string(q, 5).unwrap()), 0, 10; // Cnverges on sqrt(2)
 );
--- src/playground.rs
+++ src/playground.rs
@@ -8,6 +8,7 @@ use crate::{
         dirichlet_convolution::{DirichletConvolution, one},
         exponential::Exponential,
         prime::Primes,
+        rational_digits::rational_decimal_string,
         rational_transforms::Ratios,
         roots::{CubeRoot, SquareRoot},
         summation::{CesaroPartialSums, PartialSums},
@@ -17,8 +18,6 @@ use crate::{
     harmonic::{Harmonic, HarmonicSums},
     zeta::Zeta,
 };
-#[cfg(test)]
-use num::BigInt;
 
 crate::print_sequences!(
     print_integers, formatter "{}", sep ", ";
@@ -32,7 +31,8 @@ crate::print_sequences!(
     Zeta::new_big(3), 0, 9; // converges on Apery's constant 1.202569...
     SimpleContinuedFraction::new(Constant::new_big(1)), 0, 10; // converges on golden ratio 1.61803398875...
     SimpleContinuedFraction::new(Constant::new_big(2)), 0, 10; // converges on silver ratio 2.414213562...
-    SimpleContinuedFraction::new_periodic(crate::big!(vec![1]), crate::big!(vec![2])), 0, 10; // converges on sqrt(2) 1.41421356237...
+    SimpleContinuedFraction::new_periodic(1, &[2]), 0, 10; // converges on sqrt(2) 1.41421356237...
+    SimpleContinuedFraction::new_periodic(1, &[2]).map(|q| rational_decimal_string(q, 5).unwrap()), 0, 10; // converges on sqrt(2) 1.41421356237...
     SquareRoot::new_big(1,2), 0, 6; // converges on sqrt(1/2) 0.70710678118...
     CubeRoot::<u64>::new(2,1), 0, 5; // converges on cbrt(2) 1.25992104989...
     CesaroPartialSums::new([1,0].into_iter().cycle()), 0, 10;
