2a3646d280e7c1178e0a70b8cda8ab0c474e9fb4
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-03-04 17:01:36 -0500

    Update gray.rs

--- src/gray.rs
+++ src/gray.rs
@@ -1,35 +1,36 @@
-use num::{BigInt, Zero};
+use num::{BigInt, CheckedAdd, Integer};
+use std::ops::{BitXor, Shr};
 
 /// The Gray codes, bit sequence where each term differs from the previous by a single bit. A permutation of the integers.
-pub struct Gray {
-    ctr: BigInt,
+pub struct Gray<T> {
+    ctr: T,
 }
 
-impl Gray {
-    pub fn new_big() -> Self {
-        Self {
-            ctr: BigInt::zero(),
-        }
+impl<T: Shr<usize, Output = T> + Clone + Integer + CheckedAdd + BitXor<Output = T>> Gray<T> {
+    pub fn new() -> Self {
+        Self { ctr: T::zero() }
     }
 }
 
-impl Iterator for Gray {
-    type Item = BigInt;
+impl Gray<BigInt> {
+    pub fn new_big() -> Self {
+        Self::new()
+    }
+}
+
+impl<T: Clone + Integer + CheckedAdd + BitXor<Output = T> + Shr<usize, Output = T>> Iterator
+    for Gray<T>
+{
+    type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let out = &self.ctr ^ (&self.ctr >> 1);
-        self.ctr += 1;
-        Some(out)
-    }
-
-    fn nth(&mut self, n: usize) -> Option<Self::Item> {
-        self.ctr += n;
-        let out = &self.ctr ^ (&self.ctr >> 1);
-        self.ctr += 1;
+        let out = self.ctr.clone() ^ (self.ctr.clone() >> 1);
+        self.ctr = self.ctr.checked_add(&T::one())?;
         Some(out)
     }
 }
 
 crate::check_sequences!(
     Gray::new_big(), [0, 1, 3, 2, 6, 7, 5, 4, 12, 13];
+    Gray::<i8>::new(), [0, 1, 3, 2, 6, 7, 5, 4, 12, 13];
 );
