526c7dc2ca15639a5501d3ed1f2d5b722599461c
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-03-19 10:12:17 -0400

    Update golomb.rs

--- src/golomb.rs
+++ src/golomb.rs
@@ -1,13 +1,15 @@
 pub struct Golomb {
     ctrs: Vec<u64>,
     idx: usize,
+    ctr: u64,
 }
 
 impl Golomb {
     pub fn new() -> Self {
         Self {
-            ctrs: vec![1],
+            ctrs: vec![],
             idx: 0,
+            ctr: 1,
         }
     }
 }
@@ -16,9 +18,10 @@ impl Iterator for Golomb {
     type Item = u64;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let out = self.ctrs[self.idx];
-        self.idx += 1;
-
-        Some(out)
+        todo!()
     }
 }
+
+crate::check_sequences!(
+    Golomb::new(), [1, 2, 2, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10];
+);
