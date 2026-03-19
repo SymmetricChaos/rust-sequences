3420e27514170b13c0d5c745721273d430c43dd1
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-03-19 10:21:03 -0400

    euler pseudoprime

--- src/psuedoprime.rs
+++ src/psuedoprime.rs
@@ -37,7 +37,47 @@ impl Iterator for FermatPseudoprimes {
     }
 }
 
+/// The Euler pseudoprimes to a given base.
+/// For a base, b, the non-prime numbers, n, for which the following holds (b^((n-1)/2)) % n = 1 or -1
+pub struct EulerPseudoprimes {
+    ctr: u64,
+    base: u64,
+}
+
+impl EulerPseudoprimes {
+    /// Omly u64 output is supported.
+    pub fn new(base: u64) -> Self {
+        Self { ctr: 3, base }
+    }
+}
+
+impl Iterator for EulerPseudoprimes {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            // Odd naturals only
+            self.ctr += 2;
+
+            // Exclude primes
+            if is_prime(self.ctr) {
+                continue;
+            }
+
+            let p = pow_mod(self.base, (self.ctr - 1) / 2, self.ctr);
+
+            if p == 1 || p == self.ctr - 1 {
+                break;
+            }
+        }
+
+        Some(self.ctr)
+    }
+}
+
 crate::check_sequences!(
     FermatPseudoprimes::new(2), [341, 561, 645, 1105, 1387];
     FermatPseudoprimes::new(3), [91, 121, 286, 671, 703];
+    EulerPseudoprimes::new(2), [341, 561, 1105, 1729, 1905];
+    EulerPseudoprimes::new(3), [121, 703, 1541, 1729, 1891];
 );
