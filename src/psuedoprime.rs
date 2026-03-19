fe89417bccd59c49fba95344cdba2e12382e7d90
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-03-19 19:55:57 -0400

    Update psuedoprime.rs

--- src/psuedoprime.rs
+++ src/psuedoprime.rs
@@ -1,7 +1,7 @@
 use crate::utils::{divisibility::is_prime, exp_by_squaring::pow_mod};
 
 /// The Fermat pseudoprimes to a given base.
-/// For a base, b, the non-prime numbers, n, for which the following holds (b^(n-1)) % n = 1
+/// For a base, b, the non-prime positive integers, n, for which the following holds (b^(n-1)) % n = 1
 pub struct FermatPseudoprimes {
     ctr: u64,
     base: u64,
@@ -19,9 +19,12 @@ impl Iterator for FermatPseudoprimes {
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
-            // If the base == 2 this can be 2 in order to skip evens
-            // A number divisible by the base is never a pseudoprime to that base
             self.ctr += 1;
+
+            // Exclude values that are not co-prime to the base
+            if self.ctr % self.base == 0 {
+                continue;
+            }
 
             // Exclude primes
             if is_prime(self.ctr) {
@@ -38,8 +41,7 @@ impl Iterator for FermatPseudoprimes {
 }
 
 /// The Euler pseudoprimes to a given base.
-/// For a base, b, the odd non-prime numbers, n, for which the following holds (b^((n-1)/2)) % n = (1 or n-1)
-/// This condition is twice as strong as for a Fermat pseudoprime.
+/// For a base, b, the odd non-prime positive integers, n, for which the following holds (b^((n-1)/2)) % n = (1 or n-1)
 pub struct EulerPseudoprimes {
     ctr: u64,
     base: u64,
@@ -60,6 +62,11 @@ impl Iterator for EulerPseudoprimes {
             // Odd naturals only
             self.ctr += 2;
 
+            // Exclude values that are not co-prime to the base
+            if self.ctr % self.base == 0 {
+                continue;
+            }
+
             // Exclude primes
             if is_prime(self.ctr) {
                 continue;
@@ -77,8 +84,8 @@ impl Iterator for EulerPseudoprimes {
 }
 
 crate::check_sequences!(
-    FermatPseudoprimes::new(2), [341, 561, 645, 1105, 1387];
-    FermatPseudoprimes::new(3), [91, 121, 286, 671, 703];
+    FermatPseudoprimes::new(2), [341, 561, 645, 1105, 1387, 1729, 1905, 2047, 2465, 2701, 2821];
+    FermatPseudoprimes::new(3), [91, 121, 286, 671, 703, 949, 1105, 1541, 1729, 1891, 2465, 2665];
     EulerPseudoprimes::new(2), [341, 561, 1105, 1729, 1905];
     EulerPseudoprimes::new(3), [121, 703, 1541, 1729, 1891];
 );
