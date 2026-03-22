51b2dbcae7e9bc1a26263d770e2d78bd387ac99a
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-03-21 20:52:42 -0400

    update narayana examples

--- src/narayana_general.rs
+++ src/narayana_general.rs
@@ -2,7 +2,7 @@ use std::collections::VecDeque;
 
 use num::{BigInt, CheckedAdd, One};
 
-/// A generalization of the Fibonacci and Narayana sequences. For a non-negative constant, k, the sequence begins with the number 1 repeated k+1) times. Further terms are equal to f(n-k) + f(n).
+/// A generalization of the Fibonacci and Narayana sequences. For a non-negative constant, k, the sequence begins with the number 1 repeated k+1) times. Further terms are equal to f(n) = f(n-1) + f(n-k). Any of these sequences may be prefixed by a single zero.
 /// k = 0 gives the powers of two
 /// k = 1 gives the Fibonacci sequence
 /// k = 2 gives the Narayana cow sequence
@@ -35,8 +35,10 @@ impl<T: Clone + CheckedAdd> Iterator for NarayanaGeneral<T> {
         Some(head)
     }
 }
+
 crate::check_sequences!(
-    NarayanaGeneral::<i32>::new(0), [1, 2, 4, 8, 16, 32, 64, 128];
-    NarayanaGeneral::<i32>::new(1), [1, 1, 2, 3, 5, 8, 13, 21, 34];
-    NarayanaGeneral::<i32>::new(2), [1, 1, 1, 2, 3, 4, 6, 9, 13, 19];
+    NarayanaGeneral::<i32>::new(0), [1, 2, 4, 8, 16, 32, 64, 128]; // A000079
+    NarayanaGeneral::<i32>::new(1), [1, 1, 2, 3, 5, 8, 13, 21, 34]; // A000045
+    NarayanaGeneral::<i32>::new(2), [1, 1, 1, 2, 3, 4, 6, 9, 13, 19]; // A000930
+    NarayanaGeneral::<i32>::new(3), [1, 1, 1, 1, 2, 3, 4, 5, 7, 10]; // A003269
 );
