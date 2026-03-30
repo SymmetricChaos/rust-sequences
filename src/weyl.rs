fd94396e4854be1aa2c09df3eb9ea9fca04a9d53
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-03-30 10:26:02 -0400

    found weyl issues

--- src/weyl.rs
+++ src/weyl.rs
@@ -1,4 +1,4 @@
-use num::{BigInt, CheckedAdd, Integer, Zero, traits::CheckedRem};
+use num::{CheckedAdd, Integer};
 
 /// A Weyl-Marsaglia sequence starting at zero. A permutation of all integers 0..n that repeats every n values.
 pub struct WeylMarsaglia<T> {
@@ -7,11 +7,15 @@ pub struct WeylMarsaglia<T> {
     modulus: T,
 }
 
-impl<T: CheckedAdd + CheckedRem + Clone + Integer> WeylMarsaglia<T> {
+impl<T: CheckedAdd + Clone + Integer> WeylMarsaglia<T> {
     /// Create a new sequence with the given step and modulus.
     /// Panics if step is not coprime to modulus.
     pub fn new(step: T, modulus: T) -> Self {
-        assert!(step.gcd(&modulus) == T::one());
+        assert!(modulus != T::zero(), "modulus cannot be zero");
+        assert!(
+            step.gcd(&modulus) == T::one(),
+            "step must be copriem to modulus"
+        );
         Self {
             n: T::zero(),
             step,
@@ -20,32 +24,29 @@ impl<T: CheckedAdd + CheckedRem + Clone + Integer> WeylMarsaglia<T> {
     }
 }
 
-impl WeylMarsaglia<BigInt> {
-    /// Create a new sequence with the given step and modulus.
-    /// Panics if step is not coprime to modulus.
-    pub fn new_big<N>(step: N, modulus: N) -> Self
-    where
-        BigInt: From<N>,
-    {
-        Self {
-            n: BigInt::zero(),
-            step: BigInt::from(step),
-            modulus: BigInt::from(modulus),
-        }
-    }
-}
+// impl WeylMarsaglia<BigInt> {
+//     /// Create a new sequence with the given step and modulus.
+//     /// Panics if step is not coprime to modulus.
+//     pub fn new_big<N>(step: N, modulus: N) -> Self
+//     where
+//         BigInt: From<N>,
+//     {
+//         Self::new(BigInt::from(step), BigInt::from(modulus))
+//     }
+// }
 
-impl<T: CheckedAdd + CheckedRem + Clone> Iterator for WeylMarsaglia<T> {
+impl<T: CheckedAdd + Clone + Integer> Iterator for WeylMarsaglia<T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
         let out = self.n.clone();
         self.n = self.n.checked_add(&self.step)?;
-        self.n = self.n.checked_rem(&self.modulus)?;
+        self.n = self.n.clone() % self.modulus.clone();
         Some(out)
     }
 }
 
 crate::print_sequences!(
     WeylMarsaglia::new(5, 16), 17;
+    // WeylMarsaglia::new_big(5, 16), 17;
 );
