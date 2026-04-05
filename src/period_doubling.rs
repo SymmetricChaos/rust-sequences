471d1b614b582d2c1fd7e552103391074fe23ca2
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-04-05 08:47:03 -0400

    Update period_doubling.rs

--- src/period_doubling.rs
+++ src/period_doubling.rs
@@ -1,24 +1,43 @@
-use std::marker::PhantomData;
+use crate::padic_valuation::PadicValuation;
+use num::{BigInt, CheckedAdd, Integer};
+use std::ops::Rem;
 
-/// The period double sequence.
+/// The period doubling sequence.
 ///
 /// 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0...
 pub struct PeriodDoubling<T> {
-    _phantom: PhantomData<T>,
+    two_adic_val: PadicValuation<T>,
 }
 
-impl<T> PeriodDoubling<T> {
+impl<T: Clone + CheckedAdd + Integer> PeriodDoubling<T> {
     pub fn new() -> Self {
         Self {
-            _phantom: PhantomData,
+            two_adic_val: PadicValuation::new(T::one() + T::one()),
         }
     }
 }
 
-impl<T> Iterator for PeriodDoubling<T> {
+impl PeriodDoubling<BigInt> {
+    pub fn new_big() -> Self {
+        Self::new()
+    }
+}
+
+impl<T: Clone + CheckedAdd + Integer + Rem> Iterator for PeriodDoubling<T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        let n = self.two_adic_val.next()?;
+        Some(n % (T::one() + T::one()))
     }
 }
+
+crate::check_sequences!(
+    PeriodDoubling::new_big(),
+    [
+        0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1,
+        0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0,
+        0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1,
+        0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0
+    ];
+);
