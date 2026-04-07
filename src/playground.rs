830905ec770fe80cdd27266403e76365f1cf7f49
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-04-07 10:56:18 -0400

    Update playground.rs

--- src/playground.rs
+++ src/playground.rs
@@ -5,7 +5,7 @@ use crate::{
         continued_fraction::SimpleContinuedFraction,
         exponential::Exponential,
         prime::Primes,
-        rational_digits::rational_decimal_string as dec,
+        rational_digits::rational_decimal_string as to_dec,
         roots::{CubeRoot, SquareRoot},
     },
     dirichlet_convolution::{DirichletConvolution, one},
@@ -18,7 +18,7 @@ use crate::{
         differences::AbsDiffs,
         ordinal::OrdinalTransform,
         rational::Ratios,
-        sums::{CesaroPartialSums, PartialSums},
+        sums::{AlternatingPartialSums, CesaroPartialSums, PartialSums},
     },
     zeta::Zeta,
 };
@@ -26,6 +26,7 @@ use crate::{
 crate::print_sequences!(
     print_integers;
     PartialSums::new(Harmonic::new_big()), 10;
+    AlternatingPartialSums::new(Harmonic::new_big()), 10;
     HarmonicNumbers::<i64>::new(), 10;
     Ratios::new(Primes::new_big(),Fibonacci::new_big().skip(1)), 10;
     AbsDiffs::new(Primes::new_big()), 10;
@@ -33,14 +34,14 @@ crate::print_sequences!(
     Boustrophedon::new(Constant::new(1)), 10;
     Exponential::new_big(1,1), 8; // converges on e
     Zeta::new_big(3), 9; // converges on Apery's constant 1.202569...
-    SimpleContinuedFraction::new(Constant::new(1)), 10; // converges on golden ratio 1.61803398875...
-    SimpleContinuedFraction::new(Constant::new(2)), 10; // converges on silver ratio 2.414213562...
-    SimpleContinuedFraction::new_periodic(&[1], &[2]), 10; // converges on sqrt(2) 1.41421356237...
-    SimpleContinuedFraction::new_periodic(&[1], &[2]).map(|q| dec(q, 5).unwrap()), 10; // converges on sqrt(2) 1.41421356237...
-    SquareRoot::new_big(1,2), 6; // converges on sqrt(1/2) 0.70710678118...
-    CubeRoot::<u64>::new(2,1), 5; // converges on cbrt(2) 1.25992104989...
+    SimpleContinuedFraction::new(Constant::new(1)).map(|q| to_dec(q, 5).unwrap()), 10; // converges on golden ratio 1.61803398875...
+    SimpleContinuedFraction::new(Constant::new(2)).map(|q| to_dec(q, 5).unwrap()), 10; // converges on silver ratio 2.414213562...
+    SimpleContinuedFraction::new_periodic(&[1], &[2]).map(|q| to_dec(q, 5).unwrap()), 10; // converges on sqrt(2) 1.41421356237...
+    SquareRoot::new_big(1,2).map(|q| to_dec(q, 5).unwrap()), 6; // converges on sqrt(1/2) 0.70710678118...
+    CubeRoot::new_big(2,1).map(|q| to_dec(q, 5).unwrap()), 6; // converges on cbrt(2) 1.25992104989...
     CesaroPartialSums::new([1,0].into_iter().cycle()), 10;
     DirichletConvolution::new_big(one,one), 10; // identical to number of divisors function
     BoustrophedonTriangle::new(Primes::new_big()), 5, "{:?}", "\n";
+    Ruler::<u32>::new(), 20;
     OrdinalTransform::new(Ruler::<u32>::new()), 20;
 );
