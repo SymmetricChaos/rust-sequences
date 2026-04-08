2b769fcf620b1c51f5082f7f1d2f1f219b21cf37
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-04-08 07:56:51 -0400

    Update products.rs

--- src/sequence_transforms/products.rs
+++ src/sequence_transforms/products.rs
@@ -29,38 +29,3 @@ impl<T: CheckedMul + Clone> Iterator for PartialProds<T> {
         Some(out)
     }
 }
-
-/// Sequence of alternating partial products. Returns None if overflow occurs or sequence ends.
-pub struct AlternatingPartialProds<T> {
-    prod: T,
-    iter: Box<dyn Iterator<Item = T>>,
-    positive: bool,
-}
-
-impl<T: CheckedMul + Clone + Neg + One> AlternatingPartialProds<T> {
-    pub fn new<I>(iter: I) -> Self
-    where
-        I: Iterator<Item = T> + 'static,
-    {
-        Self {
-            prod: T::one(),
-            iter: Box::new(iter),
-            positive: true,
-        }
-    }
-}
-
-impl<T: CheckedMul + Clone + Signed> Iterator for AlternatingPartialProds<T> {
-    type Item = T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let out = self.prod.clone();
-        if self.positive {
-            self.prod = self.prod.checked_mul(&self.iter.next()?)?;
-        } else {
-            self.prod = self.prod.checked_mul(&-self.iter.next()?)?;
-        }
-        self.positive = !self.positive;
-        Some(out)
-    }
-}
