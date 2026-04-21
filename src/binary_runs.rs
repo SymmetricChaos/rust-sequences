44a8b5a35bf63e5ebf476ffc6d80e269f5697e12
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-04-21 07:43:45 -0400

    Create binary_runs.rs

---
+++ src/binary_runs.rs
@@ -0,0 +1,27 @@
+use crate::gray::Gray;
+use num::PrimInt;
+
+/// For each natural number the number of "runs" of matching bits in the binary expension. Assumes that 0 has no bits and so is assigned a value of zero.
+///
+/// 0, 1, 2, 1, 2, 3, 2, 1, 2, 3, 4, 3...
+pub struct BinaryRuns<T> {
+    gray: Gray<T>,
+}
+
+impl<T: PrimInt> BinaryRuns<T> {
+    pub fn new() -> Self {
+        Self { gray: Gray::new() }
+    }
+}
+
+impl<T: PrimInt> Iterator for BinaryRuns<T> {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.gray.next().map(|n| n.count_ones())
+    }
+}
+
+crate::check_sequences!(
+    BinaryRuns::<u8>::new(), [0, 1, 2, 1, 2, 3, 2, 1, 2, 3, 4, 3, 2, 3, 2, 1, 2, 3, 4, 3, 4, 5, 4, 3, 2, 3, 4, 3, 2, 3, 2, 1, 2, 3, 4, 3, 4, 5, 4, 3, 4, 5, 6, 5, 4, 5, 4, 3, 2, 3, 4, 3, 4, 5, 4, 3, 2, 3, 4, 3, 2, 3, 2, 1, 2, 3, 4, 3, 4, 5, 4, 3, 4, 5, 6, 5, 4, 5, 4, 3, 4, 5, 6, 5, 6, 7, 6, 5, 4, 5, 6, 5, 4, 5];
+);
