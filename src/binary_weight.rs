69898e48ac1c064baf4147b9fef79e0c46caf428
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-04-28 09:25:27 -0400

    generic binary weight

--- src/binary_weight.rs
+++ src/binary_weight.rs
@@ -1,5 +1,5 @@
-use crate::core::traits::Increment;
-use num::{Integer, PrimInt};
+use crate::core::traits::{CountBits, Increment};
+use num::{BigInt, CheckedAdd, Integer};
 
 /// The binary weight of each natural number, starting from zero.
 ///
@@ -8,17 +8,23 @@ pub struct BinaryWeight<T> {
     ctr: T,
 }
 
-impl<T: Integer + PrimInt> BinaryWeight<T> {
+impl<T: Integer + CountBits> BinaryWeight<T> {
     pub fn new() -> Self {
         Self { ctr: T::zero() }
     }
 }
 
-impl<T: Integer + PrimInt> Iterator for BinaryWeight<T> {
-    type Item = u32;
+impl BinaryWeight<BigInt> {
+    pub fn new_big() -> Self {
+        Self::new()
+    }
+}
+
+impl<T: Integer + CountBits + CheckedAdd> Iterator for BinaryWeight<T> {
+    type Item = u64;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let n = Some(self.ctr.count_ones());
+        let n = Some(self.ctr.count_ones_64());
         self.ctr.incr()?;
         n
     }
@@ -26,4 +32,5 @@ impl<T: Integer + PrimInt> Iterator for BinaryWeight<T> {
 
 crate::check_sequences!(
     BinaryWeight::<u32>::new(), [0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 2, 3, 3, 4, 3, 4, 4, 5, 3];
+    BinaryWeight::new_big(), [0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 2, 3, 3, 4, 3, 4, 4, 5, 3];
 );
--- src/core/traits.rs
+++ src/core/traits.rs
@@ -1,4 +1,49 @@
-use num::{CheckedAdd, CheckedSub, One};
+use num::{BigInt, CheckedAdd, CheckedSub, One};
+
+pub trait CustomNumTraits: Increment + Decrement + CountBits {}
+
+impl<T> CustomNumTraits for T where T: CountBits + Increment + Decrement {}
+
+/// Convenience trait for counting one and zeroes.
+pub trait CountBits {
+    fn count_ones_64(&self) -> u64;
+    fn count_zeros_64(&self) -> u64;
+}
+
+impl CountBits for BigInt {
+    fn count_ones_64(&self) -> u64 {
+        let mut out = 0;
+        for i in self.iter_u64_digits() {
+            out += i.count_ones() as u64;
+        }
+        out
+    }
+
+    fn count_zeros_64(&self) -> u64 {
+        self.bits() - self.count_ones_64()
+    }
+}
+
+macro_rules! impl_count_bits {
+    ($($type:ty),+) => {
+        $(
+            impl CountBits for $type {
+                fn count_ones_64(&self) -> u64 {
+                    self.count_ones() as u64
+                }
+
+                fn count_zeros_64(&self) -> u64 {
+                    self.count_zeros() as u64
+                }
+            }
+        )+
+    };
+}
+
+impl_count_bits!(
+    i8, u8, i16, u16, i32, u32, i64, u64, i128, u128, isize, usize
+);
+
 /// Failable increment.
 pub trait Increment {
     /// Failable increment.
