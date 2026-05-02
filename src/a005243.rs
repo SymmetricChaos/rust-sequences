5e0cb47782a4bca5ac8652c8d8ad23ea24c20873
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-05-02 15:59:40 -0400

    complements to the self generating sequences

--- src/a005243.rs
+++ src/a005243.rs
@@ -1,8 +1,7 @@
 use num::{BigInt, CheckedAdd, Integer};
-use std::{
-    collections::BTreeSet,
-    fmt::{Debug, Display},
-};
+use std::collections::BTreeSet;
+
+use crate::transforms::complement::Complement;
 
 /// Starts with 1 and 2 and then every sum of sequential terms appears in ascending order.
 ///
@@ -12,7 +11,7 @@ pub struct A005243<T> {
     heap: BTreeSet<T>,
 }
 
-impl<T: Clone + Integer> A005243<T> {
+impl<T: CheckedAdd + Clone + Integer> A005243<T> {
     pub fn new() -> Self {
         Self {
             terms: vec![],
@@ -27,7 +26,7 @@ impl A005243<BigInt> {
     }
 }
 
-impl<T: CheckedAdd + Clone + Integer + Display + Debug> Iterator for A005243<T> {
+impl<T: CheckedAdd + Clone + Integer> Iterator for A005243<T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -53,10 +52,36 @@ impl<T: CheckedAdd + Clone + Integer + Display + Debug> Iterator for A005243<T> 
     }
 }
 
+/// Complement of A005243, starting from 1.
+///
+/// 4, 7, 9, 12, 13, 15, 20, 23, 26, 27, 28...
+pub struct A048973<T>(Complement<T>);
+
+impl<T: CheckedAdd + Clone + Integer + 'static> A048973<T> {
+    pub fn new() -> Self {
+        A048973(Complement::new(A005243::new(), T::one()))
+    }
+}
+
+impl A048973<BigInt> {
+    pub fn new_big() -> Self {
+        Self::new()
+    }
+}
+
+impl<T: CheckedAdd + Clone + Integer> Iterator for A048973<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next()
+    }
+}
+
 crate::check_sequences!(
     A005243::<i32>::new(), [
         1, 2, 3, 5, 6, 8, 10, 11, 14, 16, 17, 18, 19, 21, 22, 24, 25, 29, 30,
-        32, 33, 34, 35, 37, 40, 41, 43, 45, 46, 47, 49, 51, 54, 57, 58, 59, 
-        60, 62, 65, 67, 68, 69, 70, 71, 72, 73, 75, 76, 77, 78, 80, 81, 82, 
+        32, 33, 34, 35, 37, 40, 41, 43, 45, 46, 47, 49, 51, 54, 57, 58, 59,
+        60, 62, 65, 67, 68, 69, 70, 71, 72, 73, 75, 76, 77, 78, 80, 81, 82,
         84, 86, 87, 88, 90, 91, 92, 93, 94, 95, 96, 97, 99, 100];
+    A048973::<u32>::new(), [4, 7, 9, 12, 13, 15, 20, 23, 26, 27, 28, 31, 36, 38, 39, 42, 44, 48, 50, 52, 53, 55, 56, 61, 63, 64, 66, 74, 79, 83, 85, 89, 98, 101, 103, 107, 109, 114, 120, 123, 125, 128, 131, 133, 136, 144, 152, 157, 159, 160, 165, 168, 182, 184, 190, 192, 198, 203, 208, 212];
 );
--- src/a005244.rs
+++ src/a005244.rs
@@ -1,5 +1,7 @@
-use num::{BigInt, CheckedMul, Integer};
+use num::{BigInt, CheckedAdd, CheckedMul, Integer};
 use std::collections::BTreeSet;
+
+use crate::transforms::complement::Complement;
 
 /// Starting with 2 and 3 every number that can be written as (a*b)-1,  for a and b in the sequence, appears in increasing order.
 ///
@@ -9,7 +11,7 @@ pub struct A005244<T> {
     products: BTreeSet<T>,
 }
 
-impl<T: Clone + Integer> A005244<T> {
+impl<T: Clone + Integer + CheckedMul> A005244<T> {
     pub fn new() -> Self {
         Self {
             terms: vec![T::one() + T::one()],
@@ -38,6 +40,32 @@ impl<T: Clone + Integer + CheckedMul> Iterator for A005244<T> {
     }
 }
 
+/// Complement of A005244, starting from 1.
+///
+/// 1, 4, 6, 7, 8, 10, 11, 12, 13, 15, 16...
+pub struct A171413<T>(Complement<T>);
+
+impl<T: Clone + Integer + CheckedMul + CheckedAdd + 'static> A171413<T> {
+    pub fn new() -> Self {
+        A171413(Complement::new(A005244::new(), T::one()))
+    }
+}
+
+impl A171413<BigInt> {
+    pub fn new_big() -> Self {
+        Self::new()
+    }
+}
+
+impl<T: Clone + Integer + CheckedMul + CheckedAdd> Iterator for A171413<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next()
+    }
+}
+
 crate::check_sequences!(
     A005244::<u32>::new(), [2, 3, 5, 9, 14, 17, 26, 27, 33, 41, 44, 50, 51, 53, 65, 69, 77, 80, 81, 84, 87, 98, 99, 101, 105, 122, 125, 129, 131, 134, 137, 149, 152, 153, 158, 159, 161, 164, 167, 173, 194, 195, 197, 201, 204, 206, 209, 219, 230, 233, 237, 239, 242, 243, 249];
+    A171413::<u32>::new(), [1, 4, 6, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 20, 21, 22, 23, 24, 25, 28, 29, 30, 31, 32, 34, 35, 36, 37, 38, 39, 40, 42, 43, 45, 46, 47, 48, 49, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68, 70, 71, 72, 73, 74, 75, 76, 78, 79, 82, 83, 85, 86, 88];
 );
