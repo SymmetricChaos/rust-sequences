25f43e6e29ce2e6e3df0086c8e8d7b56a589aaf3
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-05-02 17:49:40 -0400

    Update totient.rs

--- src/totient.rs
+++ src/totient.rs
@@ -2,6 +2,7 @@ use num::{Integer, rational::Ratio};
 
 use crate::{
     core::traits::Increment,
+    transforms::complement::Complement,
     utils::divisibility::{cototient, divisors, is_prime, totient},
 };
 
@@ -116,7 +117,7 @@ impl Iterator for Nontotient {
     fn next(&mut self) -> Option<Self::Item> {
         loop {
             self.ctr.incr()?;
-            if self.ctr.is_odd() {
+            if self.ctr.is_odd() && self.ctr != 1 {
                 return Some(self.ctr);
             } else {
                 let m = self.ctr;
@@ -187,10 +188,61 @@ impl Iterator for EvenNontotient {
     }
 }
 
+/// The range of Euler's totient function.
+///
+///
+pub struct TotientRange {
+    ctr: u64,
+}
+
+impl TotientRange {
+    /// Only u64 output is supported.
+    pub fn new() -> Self {
+        Self { ctr: 0 }
+    }
+}
+
+impl Iterator for TotientRange {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            self.ctr.incr()?;
+            if self.ctr == 1 {
+                return Some(1);
+            }
+            if self.ctr.is_odd() {
+                // skip it
+            } else {
+                let m = self.ctr;
+                let nmax: Ratio<u64> = divisors(m)
+                    .iter()
+                    .map(|n| *n + 1)
+                    .filter(|n| is_prime(*n))
+                    .map(|n| Ratio::new(n, n - 1))
+                    .product::<Ratio<u64>>()
+                    * m;
+                let mut n = m;
+                let mut k = 0;
+                while Ratio::new(n, 1) <= nmax {
+                    if totient(n) == m {
+                        k += 1;
+                    }
+                    n += 1;
+                }
+                if k != 0 {
+                    return Some(self.ctr);
+                }
+            }
+        }
+    }
+}
+
 crate::check_sequences!(
     Totients::new(),       [1, 1, 2, 2, 4, 2, 6, 4, 6, 4, 10, 4, 12, 6, 8, 8, 16, 6, 18, 8, 12, 10, 22, 8, 20, 12, 18, 12, 28, 8, 30, 16, 20, 16, 24, 12, 36, 18, 24, 16, 40, 12, 42, 20, 24, 22, 46, 16, 42, 20, 32, 24, 52, 18, 40, 24, 36, 28, 58, 16, 60, 30, 36, 32, 48, 20, 66, 32, 44];
     Cototients::new(),     [0, 1, 1, 2, 1, 4, 1, 4, 3, 6, 1, 8, 1, 8, 7, 8, 1, 12, 1, 12, 9, 12, 1, 16, 5, 14, 9, 16, 1, 22, 1, 16, 13, 18, 11, 24, 1, 20, 15, 24, 1, 30, 1, 24, 21, 24, 1, 32, 7, 30, 19, 28, 1, 36, 15, 32, 21, 30, 1, 44, 1, 32, 27, 32, 17, 46, 1, 36, 25, 46, 1, 48, 1, 38, 35, 40, 17, 54, 1, 48, 27];
     TotientCount::new(),   [2, 3, 0, 4, 0, 4, 0, 5, 0, 2, 0, 6, 0, 0, 0, 6, 0, 4, 0, 5, 0, 2, 0, 10, 0, 0, 0, 2, 0, 2, 0, 7, 0, 0, 0, 8, 0, 0, 0, 9, 0, 4, 0, 3, 0, 2, 0, 11, 0, 0, 0, 2, 0, 2, 0, 3, 0, 2, 0, 9, 0, 0, 0, 8, 0, 2, 0, 0, 0, 2, 0, 17, 0, 0, 0, 0, 0, 2, 0, 10, 0, 2, 0, 6, 0, 0, 0, 6, 0, 0, 0, 3];
     EvenNontotient::new(), [14, 26, 34, 38, 50, 62, 68, 74, 76, 86, 90, 94, 98, 114, 118, 122, 124, 134, 142, 146, 152, 154, 158, 170, 174, 182, 186, 188, 194, 202, 206, 214, 218, 230, 234, 236, 242, 244, 246, 248, 254, 258, 266, 274, 278, 284, 286, 290, 298, 302, 304, 308, 314, 318];
     Nontotient::new(),     [3, 5, 7, 9, 11, 13, 14, 15, 17, 19, 21, 23, 25, 26, 27, 29, 31, 33, 34, 35, 37, 38, 39, 41, 43, 45, 47, 49, 50, 51, 53, 55, 57, 59, 61, 62, 63, 65, 67, 68, 69, 71, 73, 74, 75, 76, 77, 79, 81, 83, 85, 86, 87, 89, 90, 91, 93, 94, 95, 97, 98, 99, 101, 103, 105, 107];
+    TotientRange::new(),   [1, 2, 4, 6, 8, 10, 12, 16, 18, 20, 22, 24, 28, 30, 32, 36, 40, 42, 44, 46, 48, 52, 54, 56, 58, 60, 64, 66, 70, 72, 78, 80, 82, 84, 88, 92, 96, 100, 102, 104, 106, 108, 110, 112, 116, 120, 126, 128, 130, 132, 136, 138, 140, 144, 148, 150, 156, 160, 162, 164, 166, 168, 172, 176];
 );
