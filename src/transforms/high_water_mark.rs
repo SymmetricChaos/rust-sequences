9e2142ebd02b137e502acba2d94145017718c750
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-05-06 13:43:19 -0400

    high water mark

--- src/transforms/high_water_mark.rs
+++ src/transforms/high_water_mark.rs
@@ -9,32 +9,38 @@ pub struct HighWaterMark<T> {
 
 impl<T: Clone + Ord> HighWaterMark<T> {
     /// Create a high water mark sequence using the default ordering.
-    pub fn new<I>(iter: I, start: T) -> Self
+    pub fn new<I>(iter: I) -> Self
     where
         I: Iterator<Item = T> + 'static,
     {
+        let mut iter = Box::new(iter);
+        let record = iter.next().unwrap();
         Self {
-            iter: Box::new(iter),
-            record: start,
+            iter,
+            record,
             compare: Box::new(|a, b| a.cmp(b)),
         }
     }
+}
 
+impl<T: Clone> HighWaterMark<T> {
     /// Create a high water mark sequence using a custom defined order. The ordering must be a total order.
-    pub fn new_by<I, C>(iter: I, start: T, compare: C) -> Self
+    pub fn new_by<I, C>(iter: I, compare: C) -> Self
     where
         I: Iterator<Item = T> + 'static,
         C: Fn(&T, &T) -> Ordering + 'static,
     {
+        let mut iter = Box::new(iter);
+        let record = iter.next().unwrap();
         Self {
-            iter: Box::new(iter),
-            record: start,
+            iter,
+            record,
             compare: Box::new(compare),
         }
     }
 }
 
-impl<T: Clone + Ord> Iterator for HighWaterMark<T> {
+impl<T: Clone> Iterator for HighWaterMark<T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -42,11 +48,17 @@ impl<T: Clone + Ord> Iterator for HighWaterMark<T> {
             let val = self.iter.next()?;
             match (self.compare)(&val, &self.record) {
                 Ordering::Greater => {
+                    let out = self.record.clone();
                     self.record = val.clone();
-                    return Some(val);
+                    return Some(out);
                 }
                 _ => continue,
             }
         }
     }
 }
+
+crate::check_sequences!(
+    HighWaterMark::<i32>::new(crate::core::EvenIntegers::new()), [0, 2, 4, 6, 8];
+    HighWaterMark::<i32>::new_by(crate::core::EvenIntegers::new(), |a,b| b.cmp(a)), [0, -2, -4, -6];
+);
