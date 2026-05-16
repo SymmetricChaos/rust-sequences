3320fe086211bce21410d7e2efe1ead3b35f1332
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-05-16 15:23:54 -0400

    moved totient

---
+++ src/totient/mod.rs
@@ -0,0 +1,9 @@
+pub mod highly_totient;
+pub mod jordan_totient;
+pub mod reduced_totient;
+pub mod totient;
+
+pub use highly_totient::*;
+pub use jordan_totient::*;
+pub use reduced_totient::*;
+pub use totient::*;
