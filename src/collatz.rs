b9dc0ce00a03ac073ccf1d51be999a4f231bff27
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-05-16 07:26:06 -0400

    new collatz

--- src/collatz.rs
+++ src/collatz.rs
@@ -1,6 +1,7 @@
+use crate::core::traits::Increment;
 use num::{BigInt, CheckedAdd, CheckedDiv, CheckedMul, Integer};
 
-/// The values of the Collatz (aka hailstone) sequences. If a term n the next is 3n+1 if n is odd and n/2 if n is even.
+/// The values of a Collatz sequence.
 pub struct Collatz<T> {
     value: T,
 }
@@ -39,7 +40,7 @@ impl<T: Clone + CheckedAdd + CheckedMul + CheckedDiv + Integer> Iterator for Col
     }
 }
 
-/// The odd values of the Collatz (aka hailstone) sequences. This is also called the shortcut version of the sequence.
+/// The odd values of a Collatz sequence. This is also called the shortcut or reduced Collatz sequence.
 pub struct CollatzOdd<T> {
     value: T,
 }
@@ -83,7 +84,7 @@ impl<T: Clone + CheckedAdd + CheckedMul + CheckedDiv + Integer> Iterator for Col
     }
 }
 
-/// The values of the generalized Collatz (aka hailstone) sequences. The term after n is an+b if n is odd or n/2 if n is even.
+/// The values of a generalized Collatz sequence. The term after n is an+b if n is odd or n/2 if n is even.
 pub struct CollatzGeneral<T> {
     n: T,
     a: T,
@@ -122,11 +123,52 @@ impl<T: Clone + CheckedAdd + CheckedMul + CheckedDiv + Integer> Iterator for Col
     }
 }
 
+/// Number of steps to reach 1 for a Collatz sequence starting at each positive natural number. It is not known if this sequence is defined for all inputs.
+///
+/// 0, 1, 7, 2, 5, 8, 16, 3, 19, 6, 14, 9, 9...
+pub struct CollatzLength<T> {
+    ctr: T,
+}
+
+impl<T: Clone + CheckedAdd + CheckedMul + CheckedDiv + Integer> CollatzLength<T> {
+    pub fn new() -> Self {
+        Self { ctr: T::zero() }
+    }
+}
+
+impl CollatzLength<BigInt> {
+    pub fn new_big() -> Self {
+        Self::new()
+    }
+}
+
+impl<T: Clone + CheckedAdd + CheckedMul + CheckedDiv + Integer> Iterator for CollatzLength<T> {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.ctr.incr();
+        let mut steps = 0;
+
+        let mut val = self.ctr.clone();
+        while !val.is_one() {
+            if val.is_even() {
+                val = val.checked_div(&(T::one() + T::one()))?;
+            } else {
+                val = val.checked_mul(&(T::one() + T::one() + T::one()))?;
+                val = val.checked_add(&(T::one()))?;
+            }
+            steps.incr()?;
+        }
+
+        Some(steps)
+    }
+}
+
 crate::check_sequences!(
-    Collatz::new(19), [19, 58, 29, 88, 44, 22, 11, 34, 17, 52];
     Collatz::new_big(19), [19, 58, 29, 88, 44, 22, 11, 34, 17, 52];
     Collatz::new_big(27), [27, 82, 41, 124, 62, 31, 94, 47, 142, 71];
     CollatzOdd::new_big(27), [27, 41, 31, 47, 71, 107, 161, 121, 91, 137];
     CollatzOdd::new(27), [27, 41, 31, 47, 71, 107, 161, 121, 91, 137];
     Collatz::new_big(-5), [-5, -14, -7, -20, -10, -5, -14, -7, -20, -10];
+    CollatzLength::new_big(), [0, 1, 7, 2, 5, 8, 16, 3, 19, 6, 14, 9, 9, 17, 17, 4, 12, 20, 20, 7, 7, 15, 15, 10, 23, 10, 111, 18, 18, 18, 106, 5, 26, 13, 13, 21, 21, 21, 34, 8, 109, 8, 29, 16, 16, 16, 104, 11, 24, 24, 24, 11, 11, 112, 112, 19, 32, 19, 32, 19, 19, 107, 107, 6, 27, 27, 27, 14, 14, 14, 102, 22];
 );
--- src/utils/totient.rs
+++ src/utils/totient.rs
@@ -11,6 +11,12 @@ pub fn totient(n: u64) -> u64 {
         .fold(1, |acc, x| acc * (x.0.pow((x.1 - 1) as u32) * (x.0 - 1)))
 }
 
+/// Euler's cototient function. Number of positive integers not coprime to n and less than n.
+/// Defined as 0 for n == 0.
+pub fn cototient(n: u64) -> u64 {
+    n - totient(n)
+}
+
 /// Jordan's totient function.
 /// Defined as 0 for n == 0.
 pub fn jordan_totient(n: u64) -> u64 {
@@ -18,10 +24,4 @@ pub fn jordan_totient(n: u64) -> u64 {
         return 0;
     }
     todo!()
-}
-
-/// Euler's cototient function. Number of positive integers not coprime to n and less than n.
-/// Defined as 0 for n == 0.
-pub fn cototient(n: u64) -> u64 {
-    n - totient(n)
 }
