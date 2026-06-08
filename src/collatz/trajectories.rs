ca508c87b36aa3c7d5d81d1e973319aa8efe6e1d
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-06-08 17:58:16 -0400

    api updates

--- src/collatz/trajectories.rs
+++ src/collatz/trajectories.rs
@@ -30,6 +30,10 @@ impl CollatzTrajectories<BigInt> {
             ctr: BigInt::zero(),
         }
     }
+
+    pub fn flattened_big() -> impl Iterator<Item = BigInt> {
+        Self::new_big().flatten()
+    }
 }
 
 impl Iterator for CollatzTrajectories<Number> {
@@ -76,10 +80,10 @@ impl Iterator for CollatzTrajectories<BigInt> {
 }
 
 crate::check_sequences!(
-    CollatzTrajectories::new_big().flatten(), [1, 2, 1, 3, 10, 5, 16, 8, 4, 2, 1, 4, 2, 1, 5, 16, 8, 4, 2, 1, 6, 3, 10, 5, 16, 8, 4, 2, 1, 7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1, 8, 4, 2, 1, 9, 28, 14, 7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1, 10, 5, 16, 8, 4, 2, 1, 11, 34, 17, 52, 26, 13];
+    CollatzTrajectories::flattened(), [1, 2, 1, 3, 10, 5, 16, 8, 4, 2, 1, 4, 2, 1, 5, 16, 8, 4, 2, 1, 6, 3, 10, 5, 16, 8, 4, 2, 1, 7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1, 8, 4, 2, 1, 9, 28, 14, 7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1, 10, 5, 16, 8, 4, 2, 1, 11, 34, 17, 52, 26, 13];
 );
 
 crate::sample_sequences!(
     CollatzTrajectories::new().map(|x| format!("{:?}",x));
-    CollatzTrajectories::new_big().flatten();
+    CollatzTrajectories::flattened();
 );
