d0a8ec4a32574bc88535da62da4fc2dc64b9c0a9
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-06-10 11:00:20 -0400

    Update ecg.rs

--- src/ecg.rs
+++ src/ecg.rs
@@ -1,5 +1,5 @@
-use crate::Number;
-use num::integer::gcd;
+use crate::{Number, core::traits::Increment};
+use num::{BigInt, CheckedAdd, Integer, One, Zero, integer::gcd};
 use std::collections::BTreeSet;
 
 /// The ECG sequence. Starting with 1 and 2 each term is the smallest positive integer not yet used which shares a divisor with the previous term.
@@ -7,13 +7,13 @@ use std::collections::BTreeSet;
 /// ```text
 /// 1, 2, 4, 6, 3, 9, 12, 8, 10, 5, 15, 18, 14, 7, 21, 24, 16, 20, 22...
 /// ```
-pub struct Ecg {
-    used: BTreeSet<Number>, // Checking this becomes much faster than checking a Vec after a few hundred terms
-    last: Number,
-    initial_ctr: Number,
+pub struct Ecg<T> {
+    used: BTreeSet<T>, // Checking this becomes much faster than checking a Vec after a few hundred terms
+    last: T,
+    initial_ctr: T,
 }
 
-impl Ecg {
+impl Ecg<Number> {
     pub fn new() -> Self {
         Self {
             used: BTreeSet::from([1, 2]),
@@ -23,43 +23,57 @@ impl Ecg {
     }
 }
 
-impl Iterator for Ecg {
-    type Item = Number;
+#[cfg(feature = "big_int")]
+impl Ecg<BigInt> {
+    pub fn new_big() -> Self {
+        Self {
+            used: BTreeSet::from([BigInt::one(), BigInt::from(2)]),
+            last: BigInt::zero(),
+            initial_ctr: BigInt::one(),
+        }
+    }
+}
+
+impl<T: Clone + CheckedAdd + Integer> Iterator for Ecg<T> {
+    type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.last == 0 {
-            self.last = 1;
-            return Some(1);
+        if self.last.is_zero() {
+            self.last = T::one();
+            return Some(T::one());
         }
-        if self.last == 1 {
-            self.last = 2;
-            return Some(2);
+        if self.last.is_one() {
+            self.last = T::one() + T::one();
+            return Some(T::one() + T::one());
         }
 
         // Trim the btree and advance the initial counter to repeating unnecessary work
         // This slightly slows down the iterator for the first few hundred values but hugely speeds it up after the first 1000
         if self.used.contains(&self.initial_ctr) {
             loop {
-                if self.used.contains(&(self.initial_ctr + 1)) {
+                if self
+                    .used
+                    .contains(&(self.initial_ctr.checked_add(&T::one())?))
+                {
                     self.used.remove(&self.initial_ctr);
-                    self.initial_ctr += 1;
+                    self.initial_ctr.incr()?;
                 } else {
                     break;
                 }
             }
         }
 
-        let mut ctr = self.initial_ctr;
+        let mut ctr = self.initial_ctr.clone();
 
         loop {
             if !self.used.contains(&ctr) {
-                if gcd(ctr, self.last) != 1 {
-                    self.used.insert(ctr);
-                    self.last = ctr;
-                    return Some(ctr);
+                if !gcd(ctr.clone(), self.last.clone()).is_one() {
+                    self.used.insert(ctr.clone());
+                    self.last = ctr.clone();
+                    return Some(ctr.clone());
                 }
             }
-            ctr += 1;
+            ctr.incr()?;
         }
     }
 }
