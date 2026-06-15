313bed4e93aae31ba7845947a4350fb2dc8f6786
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-06-15 07:35:21 -0400

    glfsr

--- src/rngs/lfsr.rs
+++ src/rngs/lfsr.rs
@@ -1,4 +1,6 @@
-/// A Fibonacci type linear feedback shift register that shifts bits to the right (toward the least significant bit).
+use crate::rngs::UBITS;
+
+/// A Fibonacci type linear feedback shift register that shifts bits to the right (toward the least significant bit). This iterator returns the sequence of states.
 ///
 /// ```text
 /// state = 0b0000000001, taps = 0b0100001000, bits = 10
@@ -23,8 +25,36 @@ impl Iterator for FibonacciLfsr<u64> {
     fn next(&mut self) -> Option<Self::Item> {
         let out = self.state;
         let new_bit = ((self.state & self.taps).count_ones() as u64 ^ self.state) & 1;
-        self.state = ((self.state >> 1) | (new_bit << (self.bits - 1)))
-            & (!0 >> (u64::BITS as u64 - self.bits));
+        self.state =
+            ((self.state >> 1) | (new_bit << (self.bits - 1))) & (!0 >> (UBITS - self.bits));
+        Some(out)
+    }
+}
+
+pub struct GaloisLfsr<T> {
+    state: T,
+    taps: T,
+    bits: T,
+}
+
+impl GaloisLfsr<u64> {
+    pub fn new(state: u64, taps: u64, bits: u64) -> Self {
+        assert!(bits <= UBITS);
+        Self { state, taps, bits }
+    }
+}
+
+impl Iterator for GaloisLfsr<u64> {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let out = self.state;
+        let lsb = self.state & 1;
+        if lsb == 1 {
+            self.state ^= self.taps;
+        }
+        self.state = ((self.state >> 1) | (lsb << (self.bits - 1))) & (!0 >> (UBITS - self.bits));
+
         Some(out)
     }
 }
@@ -35,8 +65,18 @@ crate::check_sequences!(
         0b00001, 0b10000, 0b01000, 0b00100, 0b10010, 0b01001, 0b10100, 0b11010, 0b01101, 0b00110,
         0b10011, 0b11001, 0b11100, 0b11110, 0b11111, 0b01111, 0b00111, 0b00011
     ];
+    GaloisLfsr::new(0b10100011000000000000000001010001, 0b01000110000000000000000000000000, 32), [
+            0b10100011000000000000000001010001_u64,
+            0b11110010100000000000000000101000,
+            0b01111001010000000000000000010100,
+            0b00111100101000000000000000001010,
+            0b00011110010100000000000000000101,
+            0b10101100001010000000000000000010,
+    ];
+
 );
 
 crate::sample_sequences!(
     FibonacciLfsr::new(0b0000000001, 0b0100001000, 10);
+    GaloisLfsr::new(0b0000000001, 0b0100001000, 10);
 );
--- src/rngs/mod.rs
+++ src/rngs/mod.rs
@@ -8,16 +8,21 @@ pub use lcg::*;
 pub use lfg::*;
 pub use lfsr::*;
 
-#[cfg(target_pointer_width = "32")]
-pub const UMAX: u32 = u32::MAX;
-#[cfg(target_pointer_width = "32")]
-pub const HALFUMAX: u32 = UMAX / 2;
-#[cfg(target_pointer_width = "32")]
-pub const SQRTUMAX: u32 = UMAX.isqrt();
+// #[cfg(target_pointer_width = "32")]
+// pub const UMAX: u32 = u32::MAX;
+// #[cfg(target_pointer_width = "32")]
+// pub const HALFUMAX: u32 = UMAX / 2;
+// #[cfg(target_pointer_width = "32")]
+// pub const SQRTUMAX: u32 = UMAX.isqrt();
 
-#[cfg(target_pointer_width = "64")]
+// #[cfg(target_pointer_width = "64")]
+// pub const UMAX: u64 = u64::MAX;
+// #[cfg(target_pointer_width = "64")]
+// pub const HALFUMAX: u64 = UMAX / 2;
+// #[cfg(target_pointer_width = "64")]
+// pub const SQRTUMAX: u64 = UMAX.isqrt();
+
 pub const UMAX: u64 = u64::MAX;
-#[cfg(target_pointer_width = "64")]
 pub const HALFUMAX: u64 = UMAX / 2;
-#[cfg(target_pointer_width = "64")]
 pub const SQRTUMAX: u64 = UMAX.isqrt();
+pub const UBITS: u64 = 64;
