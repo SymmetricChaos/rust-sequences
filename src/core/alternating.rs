f6936f9866f149047a57e320ad5e300ac9c1ab31
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-06-20 06:46:41 -0400

    updates

--- src/core/alternating.rs
+++ src/core/alternating.rs
@@ -1,5 +1,5 @@
 use num::{Signed, bigint::Sign};
-use std::{array::IntoIter, iter::Cycle, marker::PhantomData};
+use std::marker::PhantomData;
 
 /// Creates commonly used iterators that alternate between two values.
 pub struct Alternating<T> {
@@ -7,37 +7,37 @@ pub struct Alternating<T> {
 }
 
 impl Alternating<bool> {
-    /// Alternate between true and false
-    pub fn true_false() -> Cycle<IntoIter<bool, 2>> {
+    /// Alternate between true and false.
+    pub fn true_false() -> impl Iterator<Item = bool> {
         [true, false].into_iter().cycle()
     }
 
-    /// Alternate between false and true
-    pub fn false_true() -> Cycle<IntoIter<bool, 2>> {
+    /// Alternate between false and true.
+    pub fn false_true() -> impl Iterator<Item = bool> {
         [false, true].into_iter().cycle()
     }
 }
 
 impl Alternating<Sign> {
-    /// Alternate between plus sign and minus sign
-    pub fn plus_minus() -> Cycle<IntoIter<Sign, 2>> {
+    /// Alternate between plus sign and minus sign.
+    pub fn plus_minus() -> impl Iterator<Item = Sign> {
         [Sign::Plus, Sign::Minus].into_iter().cycle()
     }
 
-    /// Alternate between minus sign and plus sign
-    pub fn minus_plus() -> Cycle<IntoIter<Sign, 2>> {
+    /// Alternate between minus sign and plus sign.
+    pub fn minus_plus() -> impl Iterator<Item = Sign> {
         [Sign::Minus, Sign::Plus].into_iter().cycle()
     }
 }
 
 impl<T: Signed + Clone> Alternating<T> {
-    /// Alternate between 1 and -1
-    pub fn pos_neg() -> Cycle<IntoIter<T, 2>> {
+    /// Alternate between 1 and -1.
+    pub fn pos_neg() -> impl Iterator<Item = T> {
         [T::one(), -T::one()].into_iter().cycle()
     }
 
-    /// Alternate between -1 and 1
-    pub fn neg_pos() -> Cycle<IntoIter<T, 2>> {
+    /// Alternate between -1 and 1.
+    pub fn neg_pos() -> impl Iterator<Item = T> {
         [-T::one(), T::one()].into_iter().cycle()
     }
 }
